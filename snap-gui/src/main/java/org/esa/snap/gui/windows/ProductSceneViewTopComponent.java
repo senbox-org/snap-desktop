78a64f7ec5c1e2d0dbeb344719efb927e3c24bd0
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-11-28 18:01:34 +0100

    About to implement the publishing of figure selections

--- snap-gui/src/main/java/org/esa/snap/gui/windows/ProductSceneViewTopComponent.java
+++ snap-gui/src/main/java/org/esa/snap/gui/windows/ProductSceneViewTopComponent.java
@@ -5,27 +5,49 @@
  */
 package org.esa.snap.gui.windows;
 
-import com.bc.ceres.swing.undo.support.DefaultUndoContext;
+import com.bc.ceres.swing.selection.Selection;
+import com.bc.ceres.swing.selection.SelectionChangeEvent;
+import com.bc.ceres.swing.selection.SelectionChangeListener;
+import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.Mask;
 import org.esa.beam.framework.datamodel.ProductNode;
 import org.esa.beam.framework.datamodel.ProductNodeEvent;
 import org.esa.beam.framework.datamodel.ProductNodeListenerAdapter;
+import org.esa.beam.framework.datamodel.RasterDataNode;
+import org.esa.beam.framework.datamodel.TiePointGrid;
 import org.esa.beam.framework.ui.product.ProductSceneView;
+import org.esa.snap.gui.nodes.BNode;
+import org.esa.snap.gui.nodes.MNode;
+import org.esa.snap.gui.nodes.TPGNode;
 import org.esa.snap.gui.util.DocumentTopComponent;
 import org.esa.snap.gui.util.WindowUtilities;
 import org.openide.awt.UndoRedo;
+import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.beans.IntrospectionException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * A document window which displays images
+ *
  * @author Norman
  */
-public class ProductSceneViewTopComponent extends DocumentTopComponent<ProductNode> implements UndoRedo.Provider {
+public class ProductSceneViewTopComponent extends DocumentTopComponent<ProductNode>
+        implements UndoRedo.Provider, SelectionChangeListener {
+
+    private static final Logger LOG = Logger.getLogger(ProductSceneViewTopComponent.class.getName());
 
     private final ProductSceneView view;
     private UndoRedo undoRedo;
     private final ProductNodeListenerAdapter nodeRenameHandler;
+    private Selection selection;
 
     public ProductSceneViewTopComponent(ProductSceneView view, UndoRedo undoRedo) {
         super(view.getRaster());
@@ -33,10 +55,27 @@ public class ProductSceneViewTopComponent extends DocumentTopComponent<ProductNo
         this.undoRedo = undoRedo;
         this.nodeRenameHandler = new NodeRenameHandler();
         initComponents();
-        setName(view.getRaster().getName());
-        setDisplayName(WindowUtilities.getUniqueTitle(view.getRaster().getName(), ProductSceneViewTopComponent.class));
-        setToolTipText(view.getRaster().getProduct().getName() + " - " + view.getRaster().getName());
+        RasterDataNode raster = view.getRaster();
+        setName(raster.getName());
+        setDisplayName(WindowUtilities.getUniqueTitle(raster.getName(), ProductSceneViewTopComponent.class));
+        setToolTipText(raster.getProduct().getName() + " - " + raster.getName());
         setIcon(ImageUtilities.loadImage("org/esa/snap/gui/icons/RsBandAsSwath16.gif"));
+
+        Node node = null;
+        try {
+            if (raster instanceof Mask) {
+                node = new MNode((Mask) raster, undoRedo);
+            } else if (raster instanceof Band) {
+                node = new BNode((Band) raster, undoRedo);
+            } else if (raster instanceof TiePointGrid) {
+                node = new TPGNode((TiePointGrid) raster, undoRedo);
+            }
+        } catch (IntrospectionException e) {
+            Exceptions.printStackTrace(e);
+        }
+        if (node != null) {
+            setActivatedNodes(new Node[]{node});
+        }
     }
 
     @Override
@@ -46,6 +85,15 @@ public class ProductSceneViewTopComponent extends DocumentTopComponent<ProductNo
 
     public ProductSceneView getView() {
         return view;
+    }
+
+    @Override
+    public void selectionChanged(SelectionChangeEvent event) {
+        setSelection(event.getSelection());
+    }
+
+    @Override
+    public void selectionContextChanged(SelectionChangeEvent event) {
     }
 
     @Override
@@ -61,16 +109,47 @@ public class ProductSceneViewTopComponent extends DocumentTopComponent<ProductNo
     @Override
     protected void componentActivated() {
         getDynamicContent().add(view);
+        setSelection(view.getFigureEditor().getSelectionContext().getSelection());
+        view.getFigureEditor().getSelectionContext().addSelectionChangeListener(this);
     }
 
     @Override
     protected void componentDeactivated() {
+        view.getFigureEditor().getSelectionContext().removeSelectionChangeListener(this);
+        setSelection(null);
         getDynamicContent().remove(view);
     }
 
     private void initComponents() {
         setLayout(new BorderLayout());
         add(view, BorderLayout.CENTER);
+    }
+
+    private void setSelection(Selection newSelection) {
+        // todo - something is still wrong here!
+        LOG.info(">>> setSelection: newSelection = " + newSelection);
+        Selection oldSelection = this.selection;
+        LOG.info(">>> setSelection: oldSelection = " + oldSelection);
+        Set<Object> oldSet = oldSelection != null ? new HashSet<>(Arrays.asList(oldSelection.getSelectedValues())) : Collections.emptySet();
+        Set<Object> newSet = newSelection != null ? new HashSet<>(Arrays.asList(newSelection.getSelectedValues())) : Collections.emptySet();
+        Set<Object> tmpSet = new HashSet<>(oldSet);
+        oldSet.removeAll(newSet);
+        newSet.removeAll(tmpSet);
+        for (Object o : oldSet) {
+            LOG.info(">>> setSelection: removing o = " + o);
+            getDynamicContent().remove(o);
+        }
+        for (Object o : newSet) {
+            LOG.info(">>> setSelection: adding o = " + o);
+            getDynamicContent().add(o);
+        }
+        if (oldSelection != null && oldSelection != Selection.EMPTY) {
+            getDynamicContent().remove(oldSelection);
+        }
+        if (newSelection != null && newSelection != Selection.EMPTY) {
+            getDynamicContent().add(newSelection);
+        }
+        this.selection = newSelection;
     }
 
     private class NodeRenameHandler extends ProductNodeListenerAdapter {
