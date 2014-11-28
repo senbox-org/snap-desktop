956ae052b40713be456645a5abc939298c172c43
forman <norman.fomferra@brockmann-consult.de>
2014-11-28 23:01:42 +0100

    Two new edit actions

---
+++ snap-gui/src/main/java/org/esa/snap/gui/actions/edit/SelectAllAction.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.esa.snap.gui.actions.edit;
+
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallbackSystemAction;
+
+/**
+ * @author Norman
+ */
+@ActionID(
+        category = "Edit",
+        id = "org.esa.snap.gui.actions.edit.SelectAllAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_SelectAllActionName"
+        //, key = "D-A"
+)
+@ActionReference(
+        path = "Menu/Edit",
+        position = 10000
+)
+@NbBundle.Messages({
+        "CTL_SelectAllActionName=Select &All"
+})
+public final class SelectAllAction extends CallbackSystemAction {
+    public SelectAllAction() {
+    }
+
+    protected void initialize() {
+        super.initialize();
+    }
+
+    public Object getActionMapKey() {
+        return "select-all";
+    }
+
+    public String getName() {
+        return Bundle.CTL_SelectAllActionName();
+    }
+
+    public HelpCtx getHelpCtx() {
+        return new HelpCtx("select-all");
+    }
+
+    protected String iconResource() {
+        return null;
+    }
+
+    protected boolean asynchronous() {
+        return false;
+    }
+}
