71098ad16519d776792f4952c0cf392055d6d949
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-01-16 14:31:00 +0100

    SNAP-45: New public undo for general node insertion

---
+++ snap-gui/src/main/java/org/esa/snap/gui/nodes/UndoableProductNodeInsertion.java
@@ -0,0 +1,64 @@
+package org.esa.snap.gui.nodes;
+
+import com.bc.ceres.core.Assert;
+import org.esa.beam.framework.datamodel.ProductNode;
+import org.esa.beam.framework.datamodel.ProductNodeGroup;
+import org.openide.util.NbBundle;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+
+/**
+ * An undoable deletion of a {@code ProductNode}.
+ *
+ * @param <T> The product node type.
+ * @author Norman Fomferra
+ */
+@NbBundle.Messages("LBL_UndoableProductNodeInsertionName=Add ''{0}''")
+public class UndoableProductNodeInsertion<T extends ProductNode> extends AbstractUndoableEdit {
+
+    private ProductNodeGroup<T> group;
+    private T productNode;
+    private final int index;
+
+    public UndoableProductNodeInsertion(ProductNodeGroup<T> productNodeGroup, T productNode) {
+        Assert.notNull(productNodeGroup, "group");
+        Assert.notNull(productNode, "node");
+        this.group = productNodeGroup;
+        this.productNode = productNode;
+        this.index = productNodeGroup.indexOf(productNode);
+    }
+
+    public T getProductNode() {
+        return productNode;
+    }
+
+    @Override
+    public String getPresentationName() {
+        return Bundle.LBL_UndoableProductNodeInsertionName(productNode.getName());
+    }
+
+    @Override
+    public void undo() throws CannotUndoException {
+        super.undo();
+        // todo - close all open document windows
+        group.remove(productNode);
+    }
+
+    @Override
+    public void redo() throws CannotRedoException {
+        super.redo();
+        if (index < group.getNodeCount()) {
+            group.add(productNode);
+        } else {
+            group.add(index, productNode);
+        }
+    }
+
+    @Override
+    public void die() {
+        group = null;
+        productNode = null;
+    }
+}
