4e50b9f4d93cf733d1495c75baf099e4537f9a18
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-01-19 18:37:44 +0100

    SNAP-45: Added dependency check for CloseProductAction

--- snap-gui/src/main/java/org/esa/snap/gui/actions/file/CloseProductAction.java
+++ snap-gui/src/main/java/org/esa/snap/gui/actions/file/CloseProductAction.java
@@ -5,8 +5,13 @@
  */
 package org.esa.snap.gui.actions.file;
 
+import com.bc.jexp.ParseException;
+import org.esa.beam.framework.dataio.ProductReader;
+import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.ProductNode;
+import org.esa.beam.framework.datamodel.RasterDataNode;
+import org.esa.beam.framework.datamodel.VirtualBand;
 import org.esa.snap.gui.SnapApp;
 import org.esa.snap.gui.SnapDialogs;
 import org.esa.snap.netbeans.docwin.DocumentWindow;
@@ -24,7 +29,9 @@ import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -62,25 +69,43 @@ public final class CloseProductAction extends AbstractAction {
     }
 
     public void execute() {
-        closeProducts(productSet.toArray(new Product[productSet.size()]));
+        closeProducts(new HashSet<>(productSet));
     }
 
-    static void closeProducts(Product... products) {
+    static boolean closeProducts(Set<Product> products) {
 
-        List<Product> closeList = new ArrayList<>(Arrays.asList(products));
+        List<Product> closeList = new ArrayList<>(products);
         List<Product> saveList = new ArrayList<>();
 
-        // todo - check for product dependencies, e.g. graphs, virtual bands, etc
+        Product[] products1 = SnapApp.getDefault().getProductManager().getProducts();
+        HashSet<Product> stillOpenProducts = new HashSet<>(Arrays.asList(products1));
+        stillOpenProducts.removeAll(closeList);
+
+        if (!stillOpenProducts.isEmpty()) {
+            for (Product productToBeClosed : closeList) {
+                Product firstSourceProduct = findFirstSourceProduct(productToBeClosed, stillOpenProducts);
+                if (firstSourceProduct != null) {
+                    SnapDialogs.showInformation("Close Not Possible",
+                                                MessageFormat.format(
+                                                        "Can't close product ''{0}'' because it is in use\n" +
+                                                                "by product ''{1}''.\n" +
+                                                                "Please close the latter first.",
+                                                        productToBeClosed.getName(),
+                                                        firstSourceProduct.getName()), null);
+                    return false;
+                }
+            }
+        }
 
         for (Product product : products) {
             if (product.isModified()) {
                 int i = SnapDialogs.requestDecision(Bundle.CTL_OpenProductActionName(),
-                        MessageFormat.format("Product ''{0}'' has been modified.\nDo you want to save it?", product.getName()), true, null);
+                                                    MessageFormat.format("Product ''{0}'' has been modified.\nDo you want to save it?", product.getName()), true, null);
                 if (NotifyDescriptor.YES_OPTION.equals(i)) {
                     saveList.add(product);
                 } else if (!NotifyDescriptor.NO_OPTION.equals(i)) {
                     // cancel!
-                    return;
+                    return false;
                 }
             }
         }
@@ -91,6 +116,7 @@ public final class CloseProductAction extends AbstractAction {
                 List<IOException> problems = new ArrayList<>();
                 for (Product product : saveList) {
                     // todo - save product
+                    // SaveProductAction.saveProduct(product);
                     LOG.info("Saving to " + product.getFileLocation());
                 }
                 return problems;
@@ -128,5 +154,62 @@ public final class CloseProductAction extends AbstractAction {
         };
 
         swingWorker.execute();
+        return true;
+    }
+
+    private static Product findFirstSourceProduct(Product product, Set<Product> productsToBeClosed) {
+        Product firstSourceProduct = findFirstDirectSourceProduct(product, productsToBeClosed);
+        if (firstSourceProduct != null) {
+            return firstSourceProduct;
+        }
+        return findFirstExpressionSourceProduct(product, productsToBeClosed);
+    }
+
+    private static Product findFirstDirectSourceProduct(Product product, Set<Product> productsToBeClosed) {
+        final ProductReader reader = product.getProductReader();
+        if (reader != null) {
+            final Object input = reader.getInput();
+            if (input instanceof Product) {
+                Product sourceProduct = (Product) input;
+                if (productsToBeClosed.contains(sourceProduct)) {
+                    return sourceProduct;
+                } else {
+                    return findFirstDirectSourceProduct(sourceProduct, productsToBeClosed);
+                }
+            } else {
+                if (input instanceof Product[]) {
+                    for (final Product sourceProduct : (Product[]) input) {
+                        if (productsToBeClosed.contains(sourceProduct)) {
+                            return sourceProduct;
+                        }
+                        Product indirectSourceProduct = findFirstDirectSourceProduct(sourceProduct, productsToBeClosed);
+                        if (indirectSourceProduct != null) {
+                            return indirectSourceProduct;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static Product findFirstExpressionSourceProduct(Product product, Set<Product> productsToBeClosed) {
+        Band[] bands = product.getBands();
+        for (Band band : bands) {
+            if (band instanceof VirtualBand) {
+                VirtualBand virtualBand = (VirtualBand) band;
+                try {
+                    RasterDataNode[] nodes = product.getRefRasterDataNodes(virtualBand.getExpression());
+                    for (RasterDataNode node : nodes) {
+                        if (productsToBeClosed.contains(node.getProduct())) {
+                            return node.getProduct();
+                        }
+                    }
+                } catch (ParseException e) {
+                    // ok
+                }
+            }
+        }
+        return null;
     }
 }
