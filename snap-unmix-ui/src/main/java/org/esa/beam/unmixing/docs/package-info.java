3bf645c705a8a0269106631e55bf349493afc8ff
marpet <marco.peters@brockmann-consult.de>
2015-02-12 10:49:20 +0100

    SNAP-67: changed help position of unmixing

--- snap-unmix-ui/src/main/java/org/esa/beam/unmixing/docs/package-info.java
+++ snap-unmix-ui/src/main/java/org/esa/beam/unmixing/docs/package-info.java
@@ -1,3 +1,4 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 700) package org.esa.beam.unmixing.docs;
+@HelpSetRegistration(helpSet = "help.hs", position = 800)
+package org.esa.beam.unmixing.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
