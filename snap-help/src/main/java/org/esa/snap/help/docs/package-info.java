1790b01b954d6fa77099f4bad3c4de3a1550a13c
Nicolas Ducoin <nicolas.ducoin@c-s.fr>
2015-02-13 17:44:16 +0100

    Added snap-help with - NetBeans support - rename beam to snap - external links to web browser

---
+++ snap-help/src/main/java/org/esa/snap/help/docs/package-info.java
@@ -0,0 +1,9 @@
+@HelpSetRegistration(helpSet = "snap.hs", position = 3876)
+package org.esa.snap.help.docs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.netbeans.api.javahelp.HelpSetRegistration;
