8ec1b4c7b1c0e7168b6f4a63bb940b34eee1ae9c
Nicolas Ducoin <nicolas.ducoin@c-s.fr>
2015-05-26 16:01:41 +0200

    smart-configurator-ui initial commit

---
+++ smart-configurator-ui/src/main/java/org/esa/snap/configurator/ui/PerformanceOptionsPanelController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.esa.snap.configurator.ui;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.TopLevelRegistration(
+        categoryName = "#OptionsCategory_Name_Performance",
+        iconBase = "org/esa/snap/configurator/ui/Performance32.png",
+        keywords = "#OptionsCategory_Keywords_Performance_Optim",
+        keywordsCategory = "Performance"
+)
+@org.openide.util.NbBundle.Messages({"OptionsCategory_Name_Performance=Performance", "OptionsCategory_Keywords_Performance_Optim=Performance optimization smart configurator"})
+public final class PerformanceOptionsPanelController extends OptionsPanelController {
+
+    private PerformancePanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    public void applyChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                getPanel().store();
+                changed = false;
+            }
+        });
+    }
+
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    public boolean isChanged() {
+        return changed;
+    }
+
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private PerformancePanel getPanel() {
+        if (panel == null) {
+            panel = new PerformancePanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
