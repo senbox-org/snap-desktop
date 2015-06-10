4d7bdadbce7967cd50cbdaa342c706abe90f2ffb
marpet <marco.peters@brockmann-consult.de>
2015-06-10 08:52:09 +0200

    DocumentTopComponent should not be persistent, they would need a default constructor to be persistable

--- snap-rcp/src/main/java/org/esa/snap/rcp/placemark/PlacemarkViewTopComponent.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/placemark/PlacemarkViewTopComponent.java
@@ -5,30 +5,9 @@ import org.esa.snap.framework.ui.product.ProductPlacemarkView;
 import org.esa.snap.netbeans.docwin.DocumentTopComponent;
 import org.esa.snap.netbeans.docwin.WindowUtilities;
 import org.esa.snap.rcp.windows.ProductSceneViewTopComponent;
-import org.openide.util.NbBundle;
-import org.openide.windows.TopComponent;
 
 import java.awt.BorderLayout;
 
-/**
- * @author Tonio Fincke
- */
-@TopComponent.Description(
-        preferredID = "PlacemarkViewTopComponent",
-        persistenceType = TopComponent.PERSISTENCE_ALWAYS
-)
-@TopComponent.Registration(
-        mode = "navigator",
-        openAtStartup = true,
-        position = 0
-)
-@TopComponent.OpenActionRegistration(
-        displayName = "#CTL_PlacemarkViewTopComponentName",
-        preferredID = "PlacemarkViewTopComponent"
-)
-@NbBundle.Messages({
-        "CTL_PlacemarkViewTopComponentName=PlacemarkView",
-})
 public class PlacemarkViewTopComponent extends DocumentTopComponent<VectorDataNode> {
 
     public PlacemarkViewTopComponent(VectorDataNode document) {
