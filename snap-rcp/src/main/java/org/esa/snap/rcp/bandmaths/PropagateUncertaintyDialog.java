44c0421b9aa717c42d3898b8a9c47288a9285a6f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-01 18:54:27 +0200

    SNAP-101: added relation name to Java interface and GUI

--- snap-rcp/src/main/java/org/esa/snap/rcp/bandmaths/PropagateUncertaintyDialog.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/bandmaths/PropagateUncertaintyDialog.java
@@ -2,13 +2,11 @@ package org.esa.snap.rcp.bandmaths;
 
 import com.bc.ceres.binding.PropertyContainer;
 import com.bc.ceres.binding.PropertyDescriptor;
-import com.bc.ceres.binding.ValueRange;
 import com.bc.ceres.binding.ValueSet;
 import com.bc.ceres.core.Assert;
 import com.bc.ceres.swing.binding.BindingContext;
 import com.bc.ceres.swing.binding.PropertyEditor;
 import com.bc.ceres.swing.binding.PropertyEditorRegistry;
-import com.bc.ceres.swing.binding.internal.TextFieldEditor;
 import com.bc.jexp.ParseException;
 import com.bc.jexp.Term;
 import com.bc.jexp.impl.TermDecompiler;
@@ -28,13 +26,13 @@ import org.esa.snap.util.ProductUtils;
 import org.openide.awt.UndoRedo;
 import org.openide.util.NbBundle;
 
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.GridBagConstraints;
-import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
@@ -48,6 +46,8 @@ public class PropagateUncertaintyDialog extends ModalDialog {
 
     private static final String PROPERTY_NAME_BAND_NAME = "bandName";
     private static final String PROPERTY_NAME_ORDER = "order";
+    private static final String PROPERTY_NAME_RELATION = "relation";
+
     private static final String ERROR_PREFIX = "Error: ";
 
     private final BindingContext bindingContext;
@@ -57,6 +57,9 @@ public class PropagateUncertaintyDialog extends ModalDialog {
     private String bandName;
     @SuppressWarnings("UnusedDeclaration")
     private int order;
+    @SuppressWarnings("UnusedDeclaration")
+    private String relation;
+
     private JTextArea sourceExprArea;
     private JTextArea targetExprArea;
 
@@ -81,11 +84,15 @@ public class PropagateUncertaintyDialog extends ModalDialog {
         ProductNodeGroup<Band> bandGroup = targetProduct.getBandGroup();
 
         VirtualBand uncertaintyBand = new VirtualBand(getBandName(), ProductData.TYPE_FLOAT32, width, height, uncertaintyExpression);
-        bandGroup.add(uncertaintyBand);
         uncertaintyBand.setDescription("Uncertainty propagated from band " + sourceBand.getName() + " = " + sourceBand.getExpression());
         uncertaintyBand.setUnit(sourceBand.getUnit());
+        uncertaintyBand.setNoDataValue(Double.NaN);
+        uncertaintyBand.setNoDataValueUsed(true);
+        uncertaintyBand.setValidPixelExpression(sourceBand.getValidPixelExpression());
         ProductUtils.copySpectralBandProperties(sourceBand, uncertaintyBand);
-        sourceBand.addAncillaryVariable(uncertaintyBand, "uncertainty");
+
+        bandGroup.add(uncertaintyBand);
+        sourceBand.addAncillaryVariable(uncertaintyBand, relation);
 
         UndoRedo.Manager undoManager = SnapApp.getDefault().getUndoManager(targetProduct);
         if (undoManager != null) {
@@ -101,7 +108,7 @@ public class PropagateUncertaintyDialog extends ModalDialog {
 
     private String generateUncertaintyExpression() throws ParseException, UnsupportedOperationException {
         StandardUncertaintyGenerator propagator = new StandardUncertaintyGenerator(order, false);
-        Term term = propagator.generateUncertainty(sourceBand.getProduct(), sourceBand.getExpression());
+        Term term = propagator.generateUncertainty(sourceBand.getProduct(), relation, sourceBand.getExpression());
         return new TermDecompiler().decompile(term);
     }
 
@@ -142,20 +149,30 @@ public class PropagateUncertaintyDialog extends ModalDialog {
         targetExprArea.setEditable(true);
         updateTargetExprArea();
 
+        final JComboBox<String> comboBox = new JComboBox<>(new String[]{"uncertainty", "standard_deviation", "error"});
+        comboBox.setEditable(true);
+        bindingContext.bind(PROPERTY_NAME_RELATION, comboBox);
 
         gbc.gridy = ++line;
         components = createComponents(PROPERTY_NAME_BAND_NAME);
         GridBagUtils.addToPanel(panel, components[1], gbc,
-                                "weightx=0, insets.top=3, gridwidth=1, fill=HORIZONTAL, anchor=WEST");
+                                "weightx=0, insets.right=3, insets.top=3, gridwidth=1, fill=HORIZONTAL, anchor=WEST");
         GridBagUtils.addToPanel(panel, components[0], gbc,
-                                "weightx=1, insets.top=3, gridwidth=2, fill=HORIZONTAL, anchor=WEST");
+                                "weightx=1, insets.right=0, insets.top=3, gridwidth=2, fill=HORIZONTAL, anchor=WEST");
 
         gbc.gridy = ++line;
         components = createComponents(PROPERTY_NAME_ORDER);
         GridBagUtils.addToPanel(panel, components[1], gbc,
-                                "weightx=0, insets.top=3, gridwidth=1, fill=HORIZONTAL, anchor=WEST");
+                                "weightx=0, insets.right=3, insets.top=3, gridwidth=1, fill=HORIZONTAL, anchor=WEST");
         GridBagUtils.addToPanel(panel, components[0], gbc,
-                                "weightx=1, insets.top=3, gridwidth=2, fill=HORIZONTAL, anchor=WEST");
+                                "weightx=1, insets.right=0, insets.top=3, gridwidth=2, fill=HORIZONTAL, anchor=WEST");
+
+        gbc.gridy = ++line;
+        PropertyDescriptor relationDescriptor = bindingContext.getPropertySet().getDescriptor(PROPERTY_NAME_RELATION);
+        GridBagUtils.addToPanel(panel, new JLabel(relationDescriptor.getDisplayName() + ":"), gbc,
+                                "weightx=0, insets.right=3, insets.top=3, gridwidth=1, fill=HORIZONTAL, anchor=WEST");
+        GridBagUtils.addToPanel(panel, comboBox, gbc,
+                                "weightx=1, insets.right=0, insets.top=3, gridwidth=2, fill=HORIZONTAL, anchor=WEST");
 
         gbc.gridy = ++line;
         GridBagUtils.addToPanel(panel, new JLabel("Source expression:"), gbc,
@@ -202,15 +219,24 @@ public class PropagateUncertaintyDialog extends ModalDialog {
 
         descriptor = container.getDescriptor(PROPERTY_NAME_ORDER);
         descriptor.setDisplayName("Order of Taylor polynomial");
-        descriptor.setDescription("The number of Taylor series expansion terms used.");
+        descriptor.setDescription("The number of Taylor series expansion terms used for the Standard Combined Uncertainty (GUM 1995).");
         descriptor.setDefaultValue(1);
         descriptor.setValueSet(new ValueSet(new Integer[]{1, 2, 3}));
 
+        descriptor = container.getDescriptor(PROPERTY_NAME_RELATION);
+        descriptor.setDisplayName("Relation name of ancillary bands");
+        descriptor.setDescription("Relation  name of ancillary variables that represent uncertainties (NetCDF-U 'rel' attribute).");
+        descriptor.setDefaultValue("uncertainty");
+        descriptor.setNotNull(true);
+        descriptor.setNotEmpty(true);
+
         container.setDefaultValues();
 
-        context.addPropertyChangeListener(PROPERTY_NAME_ORDER, evt -> {
+        PropertyChangeListener targetExprUpdater = evt -> {
             updateTargetExprArea();
-        });
+        };
+        context.addPropertyChangeListener(PROPERTY_NAME_ORDER, targetExprUpdater);
+        context.addPropertyChangeListener(PROPERTY_NAME_RELATION, targetExprUpdater);
 
         return context;
     }
