ae2ed0d059b507efaecf6924af77b5a1e8c45653
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2015-07-13 14:26:15 +0200

    removed todos

--- snap-rcp/src/main/java/org/esa/snap/rcp/nodes/PNGroupingGroup.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/nodes/PNGroupingGroup.java
@@ -93,7 +93,6 @@ abstract class PNGroupingGroup extends PNGroup<Object> {
             }
             ProductNodeGroup<Band>[] autogroupingNodes = new ProductNodeGroup[autoGrouping.size() + 1];
             for (int i = 0; i < autoGrouping.size(); i++) {
-                //todo insert undoable remove action here
                 autogroupingNodes[i] = new ProductNodeGroup<>(autoGrouping.get(i)[0]);
             }
             autogroupingNodes[autoGrouping.size()] = new ProductNodeGroup<>(Bundle.LBL_BandGroupName());
@@ -154,7 +153,6 @@ abstract class PNGroupingGroup extends PNGroup<Object> {
             }
             ProductNodeGroup<TiePointGrid>[] autogroupingNodes = new ProductNodeGroup[autoGrouping.size() + 1];
             for (int i = 0; i < autoGrouping.size(); i++) {
-                //todo insert undoable remove action here
                 autogroupingNodes[i] = new ProductNodeGroup<>(autoGrouping.get(i)[0]);
             }
             autogroupingNodes[autoGrouping.size()] = new ProductNodeGroup<>(Bundle.LBL_TiePointGroupName());
