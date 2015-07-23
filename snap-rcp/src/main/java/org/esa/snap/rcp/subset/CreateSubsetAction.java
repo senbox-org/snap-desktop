6d2e98216b1d6e7203afffae6c8b913601a2cee4
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-23 14:41:20 +0200

    Fixed SNAP-150: added two new actions for spatial subset creation

---
+++ snap-rcp/src/main/java/org/esa/snap/rcp/subset/CreateSubsetAction.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.snap.rcp.subset;
+
+import com.bc.ceres.core.Assert;
+import org.esa.snap.framework.dataio.ProductSubsetDef;
+import org.esa.snap.framework.datamodel.Product;
+import org.esa.snap.framework.datamodel.ProductNode;
+import org.esa.snap.framework.ui.product.ProductSceneView;
+import org.esa.snap.framework.ui.product.ProductSubsetDialog;
+import org.esa.snap.rcp.SnapApp;
+import org.esa.snap.rcp.SnapDialogs;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
+
+import javax.swing.AbstractAction;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+
+/**
+ * This action opens a product subset dialog with the initial spatial bounds
+ * taken from the currently visible image area, if any.
+ *
+ * @author Norman Fomferra
+ */
+@ActionID(category = "Raster", id = "CreateSubsetAction")
+@ActionRegistration(
+        displayName = "#CTL_CreateSubsetAction_Name"
+)
+@ActionReferences({
+        @ActionReference(path = "Menu/Raster", position = 50)
+})
+@NbBundle.Messages({
+        "CTL_CreateSubsetAction_Name=Spatial Subset...",
+        "CTL_CreateSubsetAction_Title=Spatial Subset"
+})
+public class CreateSubsetAction extends AbstractAction {
+
+    static int subsetNumber;
+
+    private final ProductNode sourceNode;
+
+    public CreateSubsetAction(ProductNode sourceNode) {
+        this.sourceNode = sourceNode;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ignored) {
+        Product product = sourceNode.getProduct();
+        if (product != null) {
+            createSubset(product, getInitialBounds(product));
+        }
+    }
+
+    public static void createSubset(Product sourceProduct, Rectangle bounds) {
+        final String subsetName = "subset_" + CreateSubsetAction.subsetNumber + "_of_" + sourceProduct.getName();
+        final ProductSubsetDef initSubset = new ProductSubsetDef();
+        initSubset.setRegion(bounds);
+        initSubset.setNodeNames(sourceProduct.getBandNames());
+        initSubset.addNodeNames(sourceProduct.getTiePointGridNames());
+        initSubset.setIgnoreMetadata(false);
+        final ProductSubsetDialog subsetDialog = new ProductSubsetDialog(SnapApp.getDefault().getMainFrame(),
+                                                                         sourceProduct, initSubset);
+        if (subsetDialog.show() != ProductSubsetDialog.ID_OK) {
+            return;
+        }
+        final ProductSubsetDef subsetDef = subsetDialog.getProductSubsetDef();
+        if (subsetDef == null) {
+            SnapDialogs.showInformation(Bundle.CTL_CreateSubsetFromViewAction_Title(),
+                                        "No product subset created.",
+                                        null);
+            return;
+        }
+        try {
+            final Product subset = sourceProduct.createSubset(subsetDef, subsetName,
+                                                              sourceProduct.getDescription());
+            SnapApp.getDefault().getProductManager().addProduct(subset);
+            CreateSubsetAction.subsetNumber++;
+        } catch (Exception e) {
+            final String msg = "An error occurred while creating the product subset:\n" +
+                    e.getMessage();
+            SnapApp.getDefault().handleError(msg, e);
+        }
+    }
+
+    private Rectangle getInitialBounds(Product product) {
+        Rectangle bounds = null;
+        ProductSceneView view = SnapApp.getDefault().getSelectedProductSceneView();
+        if (view != null && view.getProduct() == product) {
+            bounds = view.getVisibleImageBounds();
+        }
+        return bounds;
+    }
+}
---
+++ snap-rcp/src/main/java/org/esa/snap/rcp/subset/CreateSubsetFromViewAction.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.snap.rcp.subset;
+
+import org.esa.snap.framework.ui.product.ProductSceneView;
+import org.esa.snap.rcp.SnapDialogs;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
+
+import javax.swing.AbstractAction;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+
+/**
+ * This action opens a product subset dialog with the initial spatial bounds
+ * taken from the currently visible image area.
+ *
+ * @author Norman Fomferra
+ */
+@ActionID(category = "Raster", id = "CreateSubsetFromViewAction")
+@ActionRegistration(
+        displayName = "#CTL_CreateSubsetFromViewAction_Name"
+)
+@ActionReferences({
+        @ActionReference(path = "Context/ProductSceneView", position = 100),
+})
+@NbBundle.Messages({
+        "CTL_CreateSubsetFromViewAction_Name=Spatial Subset from View...",
+        "CTL_CreateSubsetFromViewAction_Title=Spatial Subset from View"
+})
+public class CreateSubsetFromViewAction extends AbstractAction {
+
+    private final ProductSceneView view;
+
+    public CreateSubsetFromViewAction(ProductSceneView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ignored) {
+        Rectangle bounds = view.getVisibleImageBounds();
+        if (bounds == null) {
+            SnapDialogs.showWarning(Bundle.CTL_CreateSubsetFromViewAction_Title(),
+                                    "The selected area is entirely outside the product's spatial boundaries.", null);
+        }
+        CreateSubsetAction.createSubset(view.getProduct(), bounds);
+    }
+
+}
