c29f6788789c4e03eb08bbb5850fd3c479bfac53
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-08-25 15:08:13 +0200

    Removed obsolete snap-ui packages

--- snap-worldwind/src/main/java/org/esa/snap/worldwind/WWProductTreeListener.java
+++ snap-worldwind/src/main/java/org/esa/snap/worldwind/WWProductTreeListener.java
@@ -15,45 +15,39 @@
  */
 package org.esa.snap.worldwind;
 
-import org.esa.snap.framework.datamodel.Band;
-import org.esa.snap.framework.datamodel.MetadataElement;
-import org.esa.snap.framework.datamodel.Product;
-import org.esa.snap.framework.datamodel.TiePointGrid;
-import org.esa.snap.framework.ui.product.ProductTreeListener;
-import org.esa.snap.rcp.SnapApp;
-
 /**
  * listens for events from product tree
  */
-public class WWProductTreeListener implements ProductTreeListener {
-    private final WWView wwView;
-
-    public WWProductTreeListener(final WWView wwView) {
-        this.wwView = wwView;
-    }
-
-    public void productAdded(final Product product) {
-        wwView.setSelectedProduct(product);
-        wwView.setProducts(SnapApp.getDefault().getProductManager().getProducts());
-    }
-
-    public void productRemoved(final Product product) {
-        wwView.removeProduct(product);
-    }
-
-    public void productSelected(final Product product, final int clickCount) {
-        wwView.setSelectedProduct(product);
-    }
-
-    public void metadataElementSelected(final MetadataElement group, final int clickCount) {
-        wwView.setSelectedProduct(group.getProduct());
-    }
-
-    public void tiePointGridSelected(final TiePointGrid tiePointGrid, final int clickCount) {
-        wwView.setSelectedProduct(tiePointGrid.getProduct());
-    }
-
-    public void bandSelected(final Band band, final int clickCount) {
-        wwView.setSelectedProduct(band.getProduct());
-    }
+public class WWProductTreeListener {
+//        implements ProductTreeListener {
+//    private final WWView wwView;
+//
+//    public WWProductTreeListener(final WWView wwView) {
+//        this.wwView = wwView;
+//    }
+//
+//    public void productAdded(final Product product) {
+//        wwView.setSelectedProduct(product);
+//        wwView.setProducts(SnapApp.getDefault().getProductManager().getProducts());
+//    }
+//
+//    public void productRemoved(final Product product) {
+//        wwView.removeProduct(product);
+//    }
+//
+//    public void productSelected(final Product product, final int clickCount) {
+//        wwView.setSelectedProduct(product);
+//    }
+//
+//    public void metadataElementSelected(final MetadataElement group, final int clickCount) {
+//        wwView.setSelectedProduct(group.getProduct());
+//    }
+//
+//    public void tiePointGridSelected(final TiePointGrid tiePointGrid, final int clickCount) {
+//        wwView.setSelectedProduct(tiePointGrid.getProduct());
+//    }
+//
+//    public void bandSelected(final Band band, final int clickCount) {
+//        wwView.setSelectedProduct(band.getProduct());
+//    }
 }
