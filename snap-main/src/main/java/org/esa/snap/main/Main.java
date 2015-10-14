5419523b79280a221e48334cf48c8801df8d06be
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-10-14 16:21:43 +0200

    SNAP-267: Consider different user directories for SNAP Engine and Desktop

--- snap-main/src/main/java/org/esa/snap/main/Main.java
+++ snap-main/src/main/java/org/esa/snap/main/Main.java
@@ -1,15 +1,10 @@
 package org.esa.snap.main;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -19,34 +14,16 @@ import java.util.Properties;
  * {@code -J-Dnetbeans.mainclass=org.esa.snap.main.Main}
  * to {@code default_options} parameter provided in the file {@code $INSTALL_DIR/etc/snap.config}.
  * <p>
+ * The intention of this class is to initialise {@code snap.home} which will be set to the value of the
+ * NetBeans Platform system property {@code netbeans.home}, which is expected to be already set by the
+ * NetBeans Platform command-line.
+ * <p>
  * See
- * <a href="http://wiki.netbeans.org/DevFaqPlatformAppAuthStrategies">DevFaqPlatformAppAuthStrategies</a>,
- * <a href="http://wiki.netbeans.org/FaqNetbeansConf">FaqNetbeansConf</a>,
- * <a href="http://wiki.netbeans.org/FaqStartupParameters">FaqStartupParameters</a>
- * in the NetBeans wiki.
- * <p>
- * The actual intention of this class is to adjust similar configuration settings used both by the NetBeans Platform
- * and the SNAP Engine.
- * <p>
- * The system property {@code snap.home} will be set to the value of the NetBeans Platform system property
- * {@code netbeans.home}, which is expected to be already set by the NetBeans Platform command-line.
- * <p>
- * For the SNAP Desktop application the user and cache directories can be provided through
  * <ul>
- *     <li>NetBeans Platform configuration settings {@code default_userdir} and {@code default_cachedir} in the {@code $INSTALL_DIR/etc/snap.config} file</li>
- *     <li>NetBeans Platform command-line options {@code --userdir} and {@code --cachedir}</li>
+ * <li><a href="http://wiki.netbeans.org/DevFaqPlatformAppAuthStrategies">DevFaqPlatformAppAuthStrategies</a></li>
+ * <li><a href="http://wiki.netbeans.org/FaqNetbeansConf">FaqNetbeansConf</a></li>
+ * <li><a href="http://wiki.netbeans.org/FaqStartupParameters">FaqStartupParameters</a> in the NetBeans wiki.</li>
  * </ul>
- * The cache directory may not be set through NetBeans configuration at all. It defaults to {@code ${snap.userdir}/var/cache} then.
- * <p>
- * A special rule is applied on Windows OS: If
- * <ol>
- *     <li>the user directory has the special value {@code ${user.home}\AppData\Roaming\.snap} then it is redirected to {@code ${user.home}/.snap}</li>
- *     <li>the cache directory has the special value {@code ${user.home}\AppData\Roaming\.snap\var\cache} then it is redirected to {@code ${user.home}/.snap/var/cache}</li>
- * </ol>
- * <p>
- * In order to adjust similar SNAP Engine configuration properties the {@code snap.userdir} and  {@code snap.cachedir}
- * system properties are set accordingly to the NetBeans configuration.
- * This way the various methods in {@code org.esa.snap.util.SystemUtils} will make use of a common configuration.
  *
  * @author Norman Fomferra
  * @since SNAP 2
@@ -54,11 +31,6 @@ import java.util.Properties;
 public class Main {
 
     private static final String NB_MAIN_CLASS = "org.netbeans.core.startup.Main";
-    private static final String NB_CACHEDIR_OPTION = "--cachedir";
-    private static final String NB_USERDIR_OPTION = "--userdir";
-    private static final String NB_USERDIR_PROPERTY = "netbeans.user";
-    private static final String SNAP_USERDIR_PROPERTY = "snap.userdir";
-    private static final String SNAP_CACHEDIR_PROPERTY = "snap.cachedir";
 
     /**
      * A custom main entry point called from a NetBeans Platform command-line.
@@ -66,85 +38,11 @@ public class Main {
      * @param args NetBeans Platform command-line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
         System.setProperty("snap.home", Paths.get(getPropertySafe("netbeans.home")).getParent().toString());
-
-        List<String> argList = adjustUserDirArgs(args);
-        String userDir = getArg(argList, NB_USERDIR_OPTION);
-        System.setProperty(NB_USERDIR_PROPERTY, userDir);
-        System.setProperty(SNAP_USERDIR_PROPERTY, userDir);
-        System.setProperty(SNAP_CACHEDIR_PROPERTY, getArg(argList, NB_CACHEDIR_OPTION));
-
         if (Boolean.getBoolean("snap.debug")) {
-            dumpEnv(argList);
+            dumpEnv(args);
         }
-
-        runNetBeans(argList, NB_MAIN_CLASS);
-    }
-
-    static List<String> adjustUserDirArgs(String... args) {
-        List<String> argList = new ArrayList<>(Arrays.asList(args));
-
-        // User directory
-
-        Path userHome = Paths.get(getPropertySafe("user.home"));
-        String defaultUserDir = userHome.resolve(".snap").toString();
-        String userDir;
-        int userDirArgIndex = getArgIndex(argList, NB_USERDIR_OPTION);
-        if (userDirArgIndex == -1) {
-            userDir = System.getProperty(NB_USERDIR_PROPERTY, defaultUserDir);
-            argList.add(NB_USERDIR_OPTION);
-            argList.add(userDir);
-            userDirArgIndex = argList.size() - 1;
-        } else {
-            userDir = argList.get(userDirArgIndex);
-        }
-
-        // Special rule on Windows OS
-        Path nbDefaultWindowsUserDir = userHome.resolve("AppData\\Roaming\\.snap").normalize();
-        // before Java 8 update 20 the following line fails with an ArrayIndexOutOfBoundsException
-        // see https://bugs.openjdk.java.net/browse/JDK-8037945
-        boolean isWindowsDefaultUserDir = Paths.get(userDir).normalize().equals(nbDefaultWindowsUserDir);
-        if (isWindowsDefaultUserDir) {
-            userDir = defaultUserDir;
-            argList.set(userDirArgIndex, userDir);
-        }
-
-        // Cache directory
-
-        String defaultCacheDir = Paths.get(userDir).resolve("var").resolve("cache").toString();
-        int cacheDirArgIndex = getArgIndex(argList, NB_CACHEDIR_OPTION);
-        String cacheDir;
-        if (cacheDirArgIndex == -1) {
-            cacheDir = defaultCacheDir;
-            argList.add(NB_CACHEDIR_OPTION);
-            argList.add(cacheDir);
-            cacheDirArgIndex = argList.size() - 1;
-        } else {
-            cacheDir = argList.get(cacheDirArgIndex);
-        }
-
-        // Special rule on Windows OS
-        Path nbDefaultWindowsCacheDir = userHome.resolve("AppData\\Roaming\\.snap\\var\\cache").normalize();
-        boolean isWindowsDefaultCacheDir = Paths.get(cacheDir).normalize().equals(nbDefaultWindowsCacheDir);
-        if (isWindowsDefaultCacheDir) {
-            cacheDir = defaultCacheDir;
-            argList.set(cacheDirArgIndex, cacheDir);
-        }
-
-        return argList;
-    }
-
-    static String getArg(List<String> argList, String option) {
-        return argList.get(getArgIndex(argList, option));
-    }
-
-    private static int getArgIndex(List<String> argList, String option) {
-        int index = argList.indexOf(option);
-        if (index >= 0 && index < argList.size() - 1) {
-            return index + 1;
-        }
-        return -1;
+        runNetBeans(args, NB_MAIN_CLASS);
     }
 
     private static String getPropertySafe(String key) {
@@ -155,13 +53,13 @@ public class Main {
         return value;
     }
 
-    private static void dumpEnv(List<String> argList) {
+    private static void dumpEnv(String[] args) {
         System.out.println();
         System.out.println("Class: " + Main.class.getName());
         System.out.println();
         System.out.println("Arguments:");
-        for (int i = 0; i < argList.size(); i++) {
-            System.out.printf("args[%d] = \"%s\"%n", i, argList.get(i));
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("args[%d] = \"%s\"%n", i, args[i]);
         }
         System.out.println();
         System.out.println("System properties:");
@@ -180,12 +78,10 @@ public class Main {
         System.out.flush();
     }
 
-    private static void runNetBeans(List<String> argList, String mainClassName) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String[] args = argList.toArray(new String[argList.size()]);
+    private static void runNetBeans(String[] args, String mainClassName) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         Class<?> nbMainClass = classLoader.loadClass(mainClassName);
         Method nbMainMethod = nbMainClass.getDeclaredMethod("main", String[].class);
         nbMainMethod.invoke(null, new Object[]{args});
     }
-
 }
--- snap-main/src/test/java/org/esa/snap/main/MainTest.java
+++ snap-main/src/test/java/org/esa/snap/main/MainTest.java
@@ -4,96 +4,27 @@ import org.junit.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assume.assumeFalse;
-import static org.junit.Assume.assumeTrue;
 
 /**
  * Must be called from a NetBeans native platform executable.
  */
 public class MainTest {
 
-    public static final Path USER_HOME = Paths.get(System.getProperty("user.home"));
-
-    @Test
-    public void testAdaptDirArgsOnWindowsOS() throws Exception {
-        assumeTrue(System.getProperty("os.name").startsWith("Win"));
-
-        List<String> args1 = Main.adjustUserDirArgs();
-        assertEquals(USER_HOME.resolve(".snap").toString(), Main.getArg(args1, "--userdir"));
-        assertEquals(USER_HOME.resolve(".snap\\var\\cache").toString(), Main.getArg(args1, "--cachedir"));
-
-        List<String> args2 = Main.adjustUserDirArgs("--userdir", "");
-        assertEquals("", Main.getArg(args2, "--userdir"));
-        assertEquals("var\\cache", Main.getArg(args2, "--cachedir"));
-
-        List<String> args3 = Main.adjustUserDirArgs("--userdir", USER_HOME.resolve("AppData\\Roaming\\.snap").toString());
-        assertEquals(USER_HOME.resolve(".snap").toString(), Main.getArg(args3, "--userdir"));
-        assertEquals(USER_HOME.resolve(".snap\\var\\cache").toString(), Main.getArg(args3, "--cachedir"));
-
-        List<String> args4 = Main.adjustUserDirArgs("--userdir", "..\\testuser");
-        assertEquals("..\\testuser", Main.getArg(args4, "--userdir"));
-        assertEquals("..\\testuser\\var\\cache", Main.getArg(args4, "--cachedir"));
-
-        List<String> args5 = Main.adjustUserDirArgs("--userdir", "..\\testuser", "--cachedir", "..\\testcache");
-        assertEquals("..\\testuser", Main.getArg(args5, "--userdir"));
-        assertEquals("..\\testcache", Main.getArg(args5, "--cachedir"));
-
-        List<String> args6 = Main.adjustUserDirArgs("--cachedir", "..\\testcache");
-        assertEquals(USER_HOME.resolve(".snap").toString(), Main.getArg(args6, "--userdir"));
-        assertEquals("..\\testcache", Main.getArg(args6, "--cachedir"));
-    }
-
-    @Test
-    public void testAdaptDirArgsOnNonWindowsOS() throws Exception {
-        assumeFalse(System.getProperty("os.name").startsWith("Win"));
-
-        List<String> args1 = Main.adjustUserDirArgs();
-        assertEquals(USER_HOME.resolve(".snap").toString(), Main.getArg(args1, "--userdir"));
-        assertEquals(USER_HOME.resolve(".snap/var/cache").toString(), Main.getArg(args1, "--cachedir"));
-
-        List<String> args2 = Main.adjustUserDirArgs("--userdir", "");
-        assertEquals("", Main.getArg(args2, "--userdir"));
-        assertEquals("var/cache", Main.getArg(args2, "--cachedir"));
-
-        List<String> args3 = Main.adjustUserDirArgs("--userdir", USER_HOME.resolve("AppData/Roaming/.snap").toString());
-        assertEquals(USER_HOME.resolve("AppData/Roaming/.snap").toString(), Main.getArg(args3, "--userdir"));
-        assertEquals(USER_HOME.resolve("AppData/Roaming/.snap/var/cache").toString(), Main.getArg(args3, "--cachedir"));
-
-        List<String> args4 = Main.adjustUserDirArgs("--userdir", "../testuser");
-        assertEquals("../testuser", Main.getArg(args4, "--userdir"));
-        assertEquals("../testuser/var/cache", Main.getArg(args4, "--cachedir"));
-
-        List<String> args5 = Main.adjustUserDirArgs("--userdir", "../testuser", "--cachedir", "../testcache");
-        assertEquals("../testuser", Main.getArg(args5, "--userdir"));
-        assertEquals("../testcache", Main.getArg(args5, "--cachedir"));
-
-        List<String> args6 = Main.adjustUserDirArgs("--cachedir", "../testcache");
-        assertEquals(USER_HOME.resolve(".snap").toString(), Main.getArg(args6, "--userdir"));
-        assertEquals("../testcache", Main.getArg(args6, "--cachedir"));
-    }
-
     @Test
     public void testMainWithArgs() throws Exception {
         assertNull(System.getProperty("snap.home"));
-        assertNull(System.getProperty("snap.userdir"));
-        assertNull(System.getProperty("snap.cachedir"));
 
         String oldVal1 = System.setProperty("netbeans.home", "a/b/c/platform");
         try {
             try {
-                Main.main(new String[]{"--branding", "snap", "--locale","en_GB", "--cachedir", "a/b/c/cache"});
+                Main.main(new String[]{"--branding", "snap", "--locale", "en_GB"});
             } catch (ClassNotFoundException e) {
                 // ok
             }
-
             assertEquals(Paths.get("a/b/c"), Paths.get(System.getProperty("snap.home")));
-            assertEquals(USER_HOME.resolve(".snap"), Paths.get(System.getProperty("snap.userdir")));
-            assertEquals(Paths.get("a/b/c/cache"), Paths.get(System.getProperty("snap.cachedir")));
-
         } finally {
             if (oldVal1 != null) {
                 System.setProperty("netbeans.home", oldVal1);
@@ -101,45 +32,7 @@ public class MainTest {
                 System.clearProperty("netbeans.home");
             }
             System.clearProperty("snap.home");
-            System.clearProperty("snap.userdir");
-            System.clearProperty("snap.cachedir");
         }
     }
 
-    @Test
-    public void testMainWithoutArgs() throws Exception {
-        assertNull(System.getProperty("snap.home"));
-        assertNull(System.getProperty("snap.userdir"));
-        assertNull(System.getProperty("snap.cachedir"));
-
-        String oldVal1 = System.setProperty("netbeans.home", "a/b/c/platform");
-        String oldVal2 = System.setProperty("netbeans.user", "e/f/g");
-
-        try {
-
-            try {
-                Main.main(new String[0]);
-            } catch (ClassNotFoundException e) {
-                // ok
-            }
-
-            assertEquals(Paths.get("a/b/c"), Paths.get(System.getProperty("snap.home")));
-            assertEquals(Paths.get("e/f/g"), Paths.get(System.getProperty("snap.userdir")));
-            assertEquals(Paths.get("e/f/g/var/cache"), Paths.get(System.getProperty("snap.cachedir")));
-        } finally {
-            if (oldVal1 != null) {
-                System.setProperty("netbeans.home", oldVal1);
-            } else {
-                System.clearProperty("netbeans.home");
-            }
-            if (oldVal2 != null) {
-                System.setProperty("netbeans.user", oldVal2);
-            } else {
-                System.clearProperty("netbeans.user");
-            }
-            System.clearProperty("snap.home");
-            System.clearProperty("snap.userdir");
-            System.clearProperty("snap.cachedir");
-        }
-    }
 }
