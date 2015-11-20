6408c03fa48ecf2399466bb0b7f0ef481d23dfd3
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-11-20 12:54:21 +0100

    Updated API docs

---
+++ snap-rcp/src/main/java/org/esa/snap/rcp/package-info.java
@@ -0,0 +1,29 @@
+/*
+* Copyright (C) 2015 Brockmann Consult GmbH (info@brockmann-consult.de)
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 3 of the License, or (at your option)
+* any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, see http://www.gnu.org/licenses/
+*/
+
+
+
+/**
+ * Provides SNAP's Rich Client Platform (RPC) API and the implementation of the
+ * SNAP Desktop application based on the NetBeans Platform.
+ * <p>
+ * The central class in this package is the {@link org.esa.snap.rcp.SnapApp}
+ * which represents the SNAP Desktop application.
+ * <p>
+ * {@link org.esa.snap.rcp.SnapDialogs} is a utility class used to display
+ * typical user information or interaction dialogs.
+ */
+package org.esa.snap.rcp;
