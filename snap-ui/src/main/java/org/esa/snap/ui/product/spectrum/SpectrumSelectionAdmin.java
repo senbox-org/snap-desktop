63fbd225caa71628e75acc672293f1599ad1c15c
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2015-11-24 16:35:40 +0100

    made class package local

--- snap-ui/src/main/java/org/esa/snap/ui/product/spectrum/SpectrumSelectionAdmin.java
+++ snap-ui/src/main/java/org/esa/snap/ui/product/spectrum/SpectrumSelectionAdmin.java
@@ -5,7 +5,7 @@ import com.jidesoft.swing.TristateCheckBox;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpectrumSelectionAdmin {
+class SpectrumSelectionAdmin {
 
     private List<List<Boolean>> bandSelectionStates;
     private List<Integer> numbersOfSelectedBands;
