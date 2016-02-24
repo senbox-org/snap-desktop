c9cbb18ae430bf5858f38a755ff21c3ab6802c02
marpet <marco.peters@brockmann-consult.de>
2016-02-24 08:50:18 +0100

    removed default comment

--- snap-help/src/main/java/org/esa/snap/snap/help/docs/package-info.java
+++ snap-help/src/main/java/org/esa/snap/snap/help/docs/package-info.java
@@ -1,9 +1,4 @@
 @HelpSetRegistration(helpSet = "help.hs", position = 1000)
 package org.esa.snap.snap.help.docs;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import org.netbeans.api.javahelp.HelpSetRegistration;
