0156d793a6693a2f7f4231927e1cf0fccfabd9a7
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2016-02-29 18:38:30 +0100

    added resampling action

--- snap-gpf-ui/src/main/java/org/esa/snap/core/gpf/ui/resample/ResamplingAction.java
+++ snap-gpf-ui/src/main/java/org/esa/snap/core/gpf/ui/resample/ResamplingAction.java
@@ -4,26 +4,49 @@ import com.bc.ceres.binding.Property;
 import com.bc.ceres.binding.ValidationException;
 import com.bc.ceres.binding.ValueSet;
 import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.ProductManager;
 import org.esa.snap.core.datamodel.Resampler;
 import org.esa.snap.core.gpf.common.resample.ResamplingOp;
 import org.esa.snap.core.gpf.ui.DefaultSingleTargetProductDialog;
 import org.esa.snap.rcp.SnapApp;
+import org.esa.snap.rcp.actions.AbstractSnapAction;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
 
+import java.awt.event.ActionEvent;
+
 /**
+ * Action to access the Resampling Op.
+ *
  * @author Tonio Fincke
  */
+@ActionID(category = "Operators", id = "org.esa.snap.core.gpf.ui.resample.ResamplingAction")
+@ActionRegistration(displayName = "#CTL_ResamplingAction_Name")
+@ActionReference(path = "Menu/Raster", position = 60)
+@NbBundle.Messages({"CTL_ResamplingAction_Name=Resampling",
+        "CTL_ResamplingAction_Description=Uses the SNAP resampling op to resample all bands of a product to the same size.",
+        "CTL_ResamplingAction_OpName=Resampling Operator"})
 @ServiceProvider(service = Resampler.class)
-public class ResamplingOpResampler implements Resampler {
+public class ResamplingAction extends AbstractSnapAction implements Resampler {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        final Product selectedProduct = SnapApp.getDefault().getSelectedProduct(SnapApp.SelectionSourceHint.AUTO);
+        resample(selectedProduct);
+        SnapApp.getDefault().getProductManager().addListener(new PMListener());
+    }
 
     @Override
     public String getName() {
-        return "Resampling Operator";
+        return Bundle.CTL_ResamplingAction_OpName();
     }
 
     @Override
     public String getDescription() {
-        return "Uses the SNAP resampling op to resample all bands of a product to the same size";
+        return Bundle.CTL_ResamplingAction_Description();
     }
 
     @Override
@@ -33,7 +56,6 @@ public class ResamplingOpResampler implements Resampler {
 
     @Override
     public void resample(Product multiSizeProduct) {
-        //todo create dedicated resampling op ui - tf 20160224
         final DefaultSingleTargetProductDialog resamplingDialog =
                 new DefaultSingleTargetProductDialog("Resample", SnapApp.getDefault().getAppContext(), "Resampling", "resampleAction");
         resamplingDialog.setTargetProductNameSuffix("_resampled");
@@ -53,7 +75,21 @@ public class ResamplingOpResampler implements Resampler {
         resamplingDialog.show();
     }
 
-    public static void create() {
-        new ResamplingOpResampler();
+    private class PMListener implements ProductManager.Listener {
+
+        @Override
+        public void productAdded(ProductManager.Event event) {
+            updateEnableState();
+        }
+
+        @Override
+        public void productRemoved(ProductManager.Event event) {
+            updateEnableState();
+        }
+
+        private void updateEnableState() {
+            setEnabled(SnapApp.getDefault().getSelectedProduct(SnapApp.SelectionSourceHint.AUTO).isMultiSizeProduct());
+        }
+
     }
 }
