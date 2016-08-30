91252722ca44c85dd4da5b1b22bcb657aa24ecc7
marpet <marco.peters@brockmann-consult.de>
2016-08-30 14:55:15 +0200

    [SNAP-559] Can not open multiple products at once
    Currently without cancellation, unfortunately

--- snap-rcp/src/main/java/org/esa/snap/rcp/actions/file/ReadProductOperation.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/actions/file/ReadProductOperation.java
@@ -5,64 +5,114 @@ import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.rcp.SnapApp;
 import org.esa.snap.rcp.util.Dialogs;
+import org.netbeans.api.progress.ProgressHandle;
 import org.openide.util.Cancellable;
+import org.openide.util.RequestProcessor;
 
 import javax.swing.SwingUtilities;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * @author Norman
- */
-class ReadProductOperation implements Runnable, Cancellable {
+class ReadProductOperation implements Runnable {
 
     private final File file;
     private final String formatName;
-    private Boolean status;
+    private ProgressWrapper ph;
 
     public ReadProductOperation(File file, String formatName) {
         Assert.notNull(file, "file");
         Assert.notNull(formatName, "formatName");
         this.file = file;
         this.formatName = formatName;
+        ph = PhWrapper.NULL;
     }
 
-    public Boolean getStatus() {
-        return status;
+    public Cancellable createCancellable(RequestProcessor.Task task) {
+        return new Cancel(this, task);
+    }
+
+    public void attacheProgressHandle(ProgressHandle handle) {
+        ph = new PhWrapper(handle);
     }
 
     @Override
     public void run() {
         try {
+            ph.start();
+            ph.switchToIndeterminate();
             Product product = ProductIO.readProduct(file, formatName);
-            if (!Thread.interrupted()) {
+            boolean interrupted = Thread.interrupted();
+            System.out.println("Thread interrupted = " + interrupted);
+            if (!interrupted) {
                 if (product == null) {
-                    status = false;
-                    SwingUtilities.invokeLater(() -> Dialogs.showError(Bundle.LBL_NoReaderFoundText() + String.format("%nFile '%s' can not be opened.", file)));
+                    SwingUtilities.invokeLater(
+                            () -> Dialogs.showError(Bundle.LBL_NoReaderFoundText() + String.format("%nFile '%s' can not be opened.", file)));
                 } else {
-                    status = true;
                     OpenProductAction.getRecentProductPaths().add(file.getPath());
                     SwingUtilities.invokeLater(() -> SnapApp.getDefault().getProductManager().addProduct(product));
                 }
-            } else {
-                status = null;
             }
         } catch (IOException problem) {
-            status = false;
             SwingUtilities.invokeLater(() -> Dialogs.showError(Bundle.CTL_OpenProductActionName(), problem.getMessage()));
+        } finally {
+            ph.finish();
         }
     }
 
-    @Override
-    public boolean cancel() {
-        Dialogs.Answer answer = Dialogs.requestDecision(Bundle.CTL_OpenProductActionName(),
-                                                        "Do you really want to cancel the read process?",
-                                                        false, null);
-        boolean cancel = answer == Dialogs.Answer.YES;
-        if (cancel) {
-            status = null;
+    static class Cancel implements Cancellable {
+
+        private final ReadProductOperation operation;
+        private final RequestProcessor.Task task;
+
+        public Cancel(ReadProductOperation operation, RequestProcessor.Task task) {
+            this.operation = operation;
+            this.task = task;
         }
-        return cancel;
+
+        @Override
+        public boolean cancel() {
+            task.cancel();
+            operation.ph.finish();
+            return true;
+        }
     }
 
+    private static abstract class ProgressWrapper {
+
+        void start() {
+        }
+
+        void switchToIndeterminate() {
+        }
+
+        void finish() {
+        }
+
+    }
+
+    private static class PhWrapper extends ProgressWrapper {
+
+        public static ProgressWrapper NULL = new ProgressWrapper() {};
+
+        private final ProgressHandle handle;
+
+        public PhWrapper(ProgressHandle handle) {
+            this.handle = handle;
+        }
+
+        @Override
+        void start() {
+            handle.start();
+        }
+
+        @Override
+        void switchToIndeterminate() {
+            handle.switchToIndeterminate();
+        }
+
+        @Override
+        void finish() {
+            handle.finish();
+        }
+    }
 }
