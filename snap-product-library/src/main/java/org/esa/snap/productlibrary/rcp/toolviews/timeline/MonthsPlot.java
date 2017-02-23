27c9a8591e730589ea58e24f339751ba963eddfd
lveci <lveci@array.ca>
2017-02-23 16:41:47 -0500

    refactor TimelinePlot

--- snap-product-library/src/main/java/org/esa/snap/productlibrary/rcp/toolviews/timeline/MonthsPlot.java
+++ snap-product-library/src/main/java/org/esa/snap/productlibrary/rcp/toolviews/timeline/MonthsPlot.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
 package org.esa.snap.productlibrary.rcp.toolviews.timeline;
 
 import org.esa.snap.productlibrary.rcp.toolviews.model.DatabaseStatistics;
@@ -9,8 +24,9 @@ import java.util.Set;
 /**
  * Shows product counts by months
  */
-class MonthsPlot extends TimelinePlot {
+class MonthsPlot extends AbstractTimelinePlot {
 
+    private float interval;
     private final String[] monthNames = new String[]{
             "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
