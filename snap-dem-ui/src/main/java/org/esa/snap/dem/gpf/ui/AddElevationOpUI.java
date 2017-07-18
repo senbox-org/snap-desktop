24eb92871fa9113375ddb388ef9d3d05b2bfff46
jun--lu <junlu@array.ca>
2017-07-18 12:24:37 -0400

    updated to handle DEM and external DEM the same way as other operators

--- snap-dem-ui/src/main/java/org/esa/snap/dem/gpf/ui/AddElevationOpUI.java
+++ snap-dem-ui/src/main/java/org/esa/snap/dem/gpf/ui/AddElevationOpUI.java
@@ -20,17 +20,20 @@ import org.esa.snap.core.dataop.dem.ElevationModelDescriptor;
 import org.esa.snap.core.dataop.dem.ElevationModelRegistry;
 import org.esa.snap.dem.dataio.DEMFactory;
 import org.esa.snap.graphbuilder.gpf.ui.BaseOperatorUI;
+import org.esa.snap.graphbuilder.gpf.ui.OperatorUIUtils;
 import org.esa.snap.graphbuilder.gpf.ui.UIValidation;
 import org.esa.snap.graphbuilder.rcp.utils.DialogUtils;
+import org.esa.snap.rcp.util.Dialogs;
 import org.esa.snap.ui.AppContext;
 
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
 import java.util.Map;
 
 /**
@@ -39,18 +42,49 @@ import java.util.Map;
 public class AddElevationOpUI extends BaseOperatorUI {
 
     private final JComboBox<String> demName = new JComboBox<>(DEMFactory.getDEMNameList());
+    private final JComboBox demResamplingMethod = new JComboBox<>(DEMFactory.getDEMResamplingMethods());
+    private final JTextField externalDEMFile = new JTextField("");
+    private final JTextField externalDEMNoDataValue = new JTextField("");
+    private final JButton externalDEMBrowseButton = new JButton("...");
+    private final JLabel externalDEMFileLabel = new JLabel("External DEM:");
+    private final JLabel externalDEMNoDataValueLabel = new JLabel("DEM No Data Value:");
+    private Double extNoDataValue = 0.0;
+    private final DialogUtils.TextAreaKeyListener textAreaKeyListener = new DialogUtils.TextAreaKeyListener();
+    private static final String externalDEMStr = "External DEM";
 
     private final JTextField elevationBandName = new JTextField("");
-    private final JTextField externalDEM = new JTextField("");
-
-    private final JComboBox demResamplingMethod = new JComboBox<>(DEMFactory.getDEMResamplingMethods());
 
     @Override
     public JComponent CreateOpTab(String operatorName, Map<String, Object> parameterMap, AppContext appContext) {
 
+        demName.addItem(externalDEMStr);
         initializeOperatorUI(operatorName, parameterMap);
         final JComponent panel = createPanel();
         initParameters();
+
+        demName.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent event) {
+                if (((String) demName.getSelectedItem()).startsWith(externalDEMStr)) {
+                    enableExternalDEM(true);
+                } else {
+                    externalDEMFile.setText("");
+                    enableExternalDEM(false);
+                }
+            }
+        });
+        externalDEMFile.setColumns(30);
+        enableExternalDEM(((String) demName.getSelectedItem()).startsWith(externalDEMStr));
+
+        externalDEMBrowseButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                final File file = Dialogs.requestFileForOpen("External DEM File", false, null, DEMFactory.LAST_EXTERNAL_DEM_DIR_KEY);
+                externalDEMFile.setText(file.getAbsolutePath());
+                extNoDataValue = OperatorUIUtils.getNoDataValue(file);
+                externalDEMNoDataValue.setText(String.valueOf(extNoDataValue));
+            }
+        });
+
+        externalDEMNoDataValue.addKeyListener(textAreaKeyListener);
 
         return new JScrollPane(panel);
     }
@@ -67,14 +101,22 @@ public class AddElevationOpUI extends BaseOperatorUI {
                 demName.setSelectedItem(demNameParam);
             }
         }
+        demResamplingMethod.setSelectedItem(paramMap.get("demResamplingMethod"));
+
+        final File extFile = (File) paramMap.get("externalDEMFile");
+        if (extFile != null) {
+            externalDEMFile.setText(extFile.getAbsolutePath());
+            extNoDataValue = (Double) paramMap.get("externalDEMNoDataValue");
+            if (extNoDataValue != null && !textAreaKeyListener.isChangedByUser()) {
+                externalDEMNoDataValue.setText(String.valueOf(extNoDataValue));
+            }
+        }
+
         elevationBandName.setText(String.valueOf(paramMap.get("elevationBandName")));
-        externalDEM.setText(String.valueOf(paramMap.get("externalDEM")));
-        demResamplingMethod.setSelectedItem(paramMap.get("resamplingMethod"));
     }
 
     @Override
     public UIValidation validateParameters() {
-
         return new UIValidation(UIValidation.State.OK, "");
     }
 
@@ -82,9 +124,15 @@ public class AddElevationOpUI extends BaseOperatorUI {
     public void updateParameters() {
 
         paramMap.put("demName", DEMFactory.getProperDEMName((String) demName.getSelectedItem()));
+        paramMap.put("demResamplingMethod", demResamplingMethod.getSelectedItem());
+
+        final String extFileStr = externalDEMFile.getText();
+        if (!extFileStr.isEmpty()) {
+            paramMap.put("externalDEMFile", new File(extFileStr));
+            paramMap.put("externalDEMNoDataValue", Double.parseDouble(externalDEMNoDataValue.getText()));
+        }
+
         paramMap.put("elevationBandName", elevationBandName.getText());
-        paramMap.put("externalDEM", externalDEM.getText());
-        paramMap.put("resamplingMethod", demResamplingMethod.getSelectedItem());
     }
 
     private JComponent createPanel() {
@@ -96,9 +144,13 @@ public class AddElevationOpUI extends BaseOperatorUI {
         gbc.gridy++;
         DialogUtils.addComponent(contentPane, gbc, "Digital Elevation Model:", demName);
         gbc.gridy++;
-        DialogUtils.addComponent(contentPane, gbc, "External DEM:", externalDEM);
+        DialogUtils.addComponent(contentPane, gbc, externalDEMFileLabel, externalDEMFile);
+        gbc.gridx = 2;
+        contentPane.add(externalDEMBrowseButton, gbc);
         gbc.gridy++;
-        DialogUtils.addComponent(contentPane, gbc, "Resampling Method:", demResamplingMethod);
+        DialogUtils.addComponent(contentPane, gbc, externalDEMNoDataValueLabel, externalDEMNoDataValue);
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, "DEM Resampling Method:", demResamplingMethod);
         gbc.gridy++;
         DialogUtils.addComponent(contentPane, gbc, "Elevation Band Name:", elevationBandName);
         gbc.gridy++;
@@ -106,6 +158,12 @@ public class AddElevationOpUI extends BaseOperatorUI {
         DialogUtils.fillPanel(contentPane, gbc);
 
         return contentPane;
+    }
+
+    private void enableExternalDEM(boolean flag) {
+        DialogUtils.enableComponents(externalDEMFileLabel, externalDEMFile, flag);
+        DialogUtils.enableComponents(externalDEMNoDataValueLabel, externalDEMNoDataValue, flag);
+        externalDEMBrowseButton.setVisible(flag);
     }
 
 }
