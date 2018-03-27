05afe7f630c7481112041b5dc33b0f070891c227
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-03-27 15:21:22 +0200

    work on handling mouse events

--- snap-rcp/src/main/java/org/esa/snap/rcp/statistics/ScatterPlot3dJzyPanel.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/statistics/ScatterPlot3dJzyPanel.java
@@ -1,28 +1,42 @@
 package org.esa.snap.rcp.statistics;
 
+import com.jogamp.common.nio.Buffers;
 import com.jogamp.newt.event.MouseEvent;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
+import com.jogamp.opengl.glu.GLU;
 import org.jzy3d.chart.Chart;
 import org.jzy3d.chart.controllers.mouse.NewtMouseUtilities;
 import org.jzy3d.chart.controllers.mouse.camera.NewtCameraMouseController;
-import org.jzy3d.chart.factories.AWTChartComponentFactory;
+import org.jzy3d.chart.factories.NewtChartComponentFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.maths.BoundingBox3d;
 import org.jzy3d.maths.Coord2d;
 import org.jzy3d.maths.Coord3d;
+import org.jzy3d.maths.IntegerCoord2d;
 import org.jzy3d.maths.Range;
 import org.jzy3d.maths.Scale;
 import org.jzy3d.picking.PickingSupport;
 import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.primitives.pickable.Pickable;
 import org.jzy3d.plot3d.primitives.pickable.PickablePoint;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.rendering.scene.Graph;
+import org.jzy3d.plot3d.rendering.view.Camera;
 import org.jzy3d.plot3d.rendering.view.View;
+import org.jzy3d.plot3d.rendering.view.modes.CameraMode;
 import org.jzy3d.plot3d.text.DrawableTextWrapper;
 import org.jzy3d.plot3d.text.renderers.TextBitmapRenderer;
+import org.jzy3d.plot3d.transform.Transform;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 //import org.jzy3d.plot3d.primitives.pickable.PickablePoint;
 
@@ -39,10 +53,11 @@ class ScatterPlot3dJzyPanel extends JPanel {
     private Range yRange;
     private Range zRange;
     private DrawableTextWrapper textWrapper;
+    private ScatterPlot3dCameraMouseController cameraController;
 
     void init() {
-        chart = AWTChartComponentFactory.chart(Quality.Nicest, "newt");
-//        pickingSupport = new PickingSupport();
+        chart = NewtChartComponentFactory.chart(Quality.Nicest, "newt");
+        pickingSupport = new ScatterPlot3DPickingSupport();
         textWrapper = new DrawableTextWrapper("", new Coord3d(0, 0, 0),
                                               Color.BLACK, new TextBitmapRenderer());
         pickingSupport.addObjectPickedListener((vertices, picking) -> {
@@ -56,14 +71,12 @@ class ScatterPlot3dJzyPanel extends JPanel {
         scatter = new Scatter();
         scatter.setWidth(10.0f);
         chart.getScene().add(scatter);
+        chart.add(textWrapper);
         setLayout(new BorderLayout());
-        new ScatterPlot3dCameraMouseController(chart);
-//        chart.addMouseCameraController();
-//        IMousePickingController pickingController = chart.addMousePickingController(10);
-//        pickingController.setPickingSupport(pickingSupport);
+        cameraController = new ScatterPlot3dCameraMouseController(this.chart, pickingSupport);
         titleLabel = new JLabel();
         add(titleLabel, BorderLayout.NORTH);
-        add((Component) chart.getCanvas(), BorderLayout.CENTER);
+        add((Component) this.chart.getCanvas(), BorderLayout.CENTER);
     }
 
     void setChartTitle(String chartTitle) {
@@ -79,7 +92,7 @@ class ScatterPlot3dJzyPanel extends JPanel {
     void updateChart(float[] xData, float[] yData, float[] zData, double xScale, double yScale, double zScale,
                      float xMin, float xMax, float yMin, float yMax, float zMin, float zMax) {
 //        chart.clear();
-        scatter.clear();
+//        scatter.clear();
 //        pickingSupport.unRegisterAllPickableObjects();
         final int size = xData.length;
         Coord3d[] points = new Coord3d[size];
@@ -87,8 +100,8 @@ class ScatterPlot3dJzyPanel extends JPanel {
         for (int i = 0; i < size; i++) {
             points[i] = new Coord3d(xData[i] * xScale, yData[i] * yScale, zData[i] * zScale);
             PickablePoint pickablePoint = new PickablePoint(points[i]);
-//            pickingSupport.registerPickableObject(pickablePoint, points[i]);
-            colors[i] = Color.CYAN;
+            pickingSupport.registerPickableObject(pickablePoint, points[i]);
+            colors[i] = new Color(0.0f, 1.0f, 1.0f, 0.25f);
         }
         scatter.setData(points);
         scatter.setColors(colors);
@@ -97,7 +110,12 @@ class ScatterPlot3dJzyPanel extends JPanel {
         zRange = new Range(zMin, zMax);
         final BoundingBox3d box = new BoundingBox3d(xRange, yRange, zRange);
         chart.getView().setBoundManual(box);
+        System.out.println("Attempting to render ...");
         renderChart();
+    }
+
+    void toggleAnimate() {
+        cameraController.toggleAnimate();
     }
 
     void renderChart() {
@@ -106,20 +124,27 @@ class ScatterPlot3dJzyPanel extends JPanel {
 
     private class ScatterPlot3dCameraMouseController extends NewtCameraMouseController {
 
-        ScatterPlot3dCameraMouseController(Chart chart) {
+        private final PickingSupport pickingSupport;
+        private final GLU glu;
+        private boolean isAnimating;
+
+        ScatterPlot3dCameraMouseController(Chart chart, PickingSupport pickingSupport) {
             super(chart);
+            this.pickingSupport = pickingSupport;
+            glu = new GLU();
+            isAnimating = false;
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
             Coord2d mouse = new Coord2d(e.getX(), e.getY());
             // Rotate
-            if(isLeftDown(e)){
-                Coord2d move  = mouse.sub(prevMouse).div(100);
-                rotate( move );
+            if (isLeftDown(e)) {
+                Coord2d move = mouse.sub(prevMouse).div(100);
+                rotate(move);
             }
             // Shift
-            else if(isRightDown(e)){
+            else if (isRightDown(e)) {
                 final View view = chart.getView();
                 final Coord3d prevMouse3D = view.projectMouse((int) prevMouse.x, (int) prevMouse.y);
                 final Coord3d move3D = view.projectMouse((int) mouse.x, (int) mouse.y);
@@ -150,6 +175,113 @@ class ScatterPlot3dJzyPanel extends JPanel {
             zoomY(factor);
             zoomZ(factor);
         }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+            int yflip = -y + targets.get(0).getCanvas().getRendererHeight();
+            prevMouse.x = x;
+            prevMouse.y = y;// yflip;
+            View view = targets.get(0).getView();
+            GL gl = chart().getView().getCurrentGL();
+//
+            // will trigger vertex selection event to those subscribing to
+            pickingSupport.pickObjects(gl, glu, view, null, new IntegerCoord2d(x, yflip));
+            chart().getView().getCurrentContext().release();
+        }
+
+        void toggleAnimate() {
+            if (this.threadController != null) {
+                if (!isAnimating) {
+                    this.threadController.start();
+                } else {
+                    this.threadController.stop();
+                }
+            }
+            isAnimating = !isAnimating;
+        }
+    }
+
+    private class ScatterPlot3DPickingSupport extends PickingSupport {
+
+        @Override
+        public void pickObjects(GL gl, GLU glu, View view, Graph graph, IntegerCoord2d pickPoint) {
+            perf.tic();
+
+            int viewport[] = new int[4];
+            int selectBuf[] = new int[bufferSize]; // TODO: move @ construction
+            IntBuffer selectBuffer = Buffers.newDirectIntBuffer(bufferSize);
+
+
+            if (!gl.isGL2()) throw new UnsupportedOperationException();
+
+            // Prepare selection data
+            gl.getGL2().glGetIntegerv(GL.GL_VIEWPORT, viewport, 0);
+            gl.getGL2().glSelectBuffer(bufferSize, selectBuffer);
+            gl.getGL2().glRenderMode(GL2.GL_SELECT);
+            gl.getGL2().glInitNames();
+            gl.getGL2().glPushName(0);
+
+            // Retrieve view settings
+            Camera camera = view.getCamera();
+            CameraMode cMode = view.getCameraMode();
+            Coord3d viewScaling = view.getLastViewScaling();
+            Transform viewTransform = new Transform(new org.jzy3d.plot3d.transform.Scale(viewScaling));
+            double xpick = pickPoint.x;
+            double ypick = pickPoint.y;
+
+            // Setup projection matrix
+            gl.getGL2().glMatrixMode(GLMatrixFunc.GL_PROJECTION);
+            gl.getGL2().glPushMatrix();
+            {
+                gl.getGL2().glLoadIdentity();
+                // Setup picking matrix, and update view frustrum
+                glu.gluPickMatrix(xpick, ypick, brushSize, brushSize, viewport, 0);
+                camera.doShoot(gl, glu, cMode);
+
+                // Draw each pickable element in select buffer
+                gl.getGL2().glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+
+                synchronized (this) {
+                    for (Pickable pickable : pickables.values()) {
+                        setCurrentName(gl, pickable);
+                        pickable.setTransform(viewTransform);
+                        pickable.draw(gl, glu, camera);
+                        releaseCurrentName(gl);
+                    }
+                }
+                // Back to projection matrix
+                gl.getGL2().glMatrixMode(GLMatrixFunc.GL_PROJECTION);
+            }
+            gl.getGL2().glPopMatrix();
+            gl.glFlush();
+
+            // Process hits
+            int hits = gl.getGL2().glRenderMode(GL2.GL_RENDER);
+            if (hits < 1) {
+                return;
+            }
+            selectBuffer.get(selectBuf);
+            int minZ = Integer.MAX_VALUE;
+            int pickableIndex = -1;
+            for (int j = 0; j < hits; j++) {
+                if (selectBuf[4*j + 1] < minZ) {
+                    minZ = selectBuf[4*j + 1];
+                    pickableIndex = selectBuf[4*j + 3];
+                }
+            }
+            final Pickable pickable = pickables.get(pickableIndex);
+
+            // Trigger an event
+            List<Object> clickedObjects = new ArrayList<>(hits);
+            Object vertex = pickableTargets.get(pickable);
+            clickedObjects.add(vertex);
+            perf.toc();
+
+            fireObjectPicked(clickedObjects);
+        }
+
     }
 
 }
