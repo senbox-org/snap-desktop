da3fe4e24a2ee8d6754b966cf99e57b23778c6d1
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-04-23 18:42:23 +0200

    added projection scatter class

---
+++ snap-rcp/src/main/java/org/esa/snap/rcp/statistics/ScatterPlot3DProjectionScatter.java
@@ -0,0 +1,266 @@
+package org.esa.snap.rcp.statistics;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.glu.GLU;
+import org.jzy3d.maths.Coord3d;
+import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.rendering.compat.GLES2CompatUtils;
+import org.jzy3d.plot3d.rendering.view.Camera;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jogamp.opengl.GL2GL3.GL_FILL;
+
+class ScatterPlot3DProjectionScatter extends Scatter {
+
+    private static final int NUM_SEGMENTS = 12;
+    private static float[] SINES;
+    private static float[] COSINES;
+    private static List<Projector> ACTIVE_PROJECTORS;
+
+    private float scaleFactor;
+    private Projector xProjector;
+    private Projector yProjector;
+    private Projector zProjector;
+
+    ScatterPlot3DProjectionScatter() {
+        super();
+        scaleFactor = 1f;
+        xProjector = new XProjector();
+        yProjector = new YProjector();
+        zProjector = new ZProjector();
+        ACTIVE_PROJECTORS = new ArrayList<>();
+        initSinesAndCosines();
+    }
+
+    private void initSinesAndCosines() {
+        SINES = new float[NUM_SEGMENTS + 1];
+        COSINES = new float[NUM_SEGMENTS + 1];
+        float theta = (2 * (float) Math.PI) / NUM_SEGMENTS;
+        for (int i = 0; i < NUM_SEGMENTS; i++) {
+            SINES[i] = (float) Math.sin(theta * i);
+            COSINES[i] = (float) Math.cos(theta * i);
+        }
+        SINES[NUM_SEGMENTS] = SINES[0];
+        COSINES[NUM_SEGMENTS] = COSINES[0];
+    }
+
+    @Override
+    public void draw(GL gl, GLU glu, Camera cam) {
+        this.doTransform(gl, glu, cam);
+        if (gl.isGL2()) {
+            this.drawGL2(gl);
+        } else {
+            this.drawGLES2();
+        }
+
+        this.doDrawBounds(gl, glu, cam);
+    }
+
+    @Override
+    public void drawGLES2() {
+        if (coordinates != null && !ACTIVE_PROJECTORS.isEmpty()) {
+            if (colors == null)
+                GLES2CompatUtils.glColor4f(rgb.r, rgb.g, rgb.b, rgb.a);
+            for (Projector projector : ACTIVE_PROJECTORS) {
+                projector.prepareDrawing();
+            }
+            int k = 0;
+            for (Coord3d c : coordinates) {
+                GLES2CompatUtils.glDisable(GL.GL_CULL_FACE);
+                GLES2CompatUtils.glPolygonMode(GL.GL_FRONT_AND_BACK, GL_FILL);
+                if (colors != null) {
+                    GLES2CompatUtils.glColor4f(colors[k].r, colors[k].g, colors[k].b, colors[k].a);
+                    k++;
+                }
+                for (Projector projector : ACTIVE_PROJECTORS) {
+                    projector.drawGLES2(c);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void drawGL2(GL gl) {
+        if (coordinates != null && !ACTIVE_PROJECTORS.isEmpty()) {
+            if (colors == null)
+                gl.getGL2().glColor4f(rgb.r, rgb.g, rgb.b, rgb.a);
+            for (Projector projector : ACTIVE_PROJECTORS) {
+                projector.prepareDrawing();
+            }
+            int k = 0;
+            for (Coord3d c : coordinates) {
+                gl.getGL2().glDisable(GL.GL_CULL_FACE);
+                gl.getGL2().glPolygonMode(GL.GL_FRONT_AND_BACK, GL_FILL);
+                if (colors != null) {
+                    gl.getGL2().glColor4f(colors[k].r, colors[k].g, colors[k].b, colors[k].a);
+                    k++;
+                }
+                for (Projector projector : ACTIVE_PROJECTORS) {
+                    projector.drawGL2(c, gl);
+                }
+            }
+        }
+    }
+
+    void scale(float scaleFactor) {
+        this.scaleFactor *= scaleFactor;
+    }
+
+    void setEdgeCoords(float xEdgeCoord, float yEdgeCoord, float zEdgeCoord) {
+        xProjector.setEdgeCoord(xEdgeCoord);
+        yProjector.setEdgeCoord(yEdgeCoord);
+        zProjector.setEdgeCoord(zEdgeCoord);
+    }
+
+    void projectToX(boolean projectToX) {
+        if (projectToX && !ACTIVE_PROJECTORS.contains(xProjector)) {
+            ACTIVE_PROJECTORS.add(xProjector);
+        } else if (!projectToX) {
+            ACTIVE_PROJECTORS.remove(xProjector);
+        }
+    }
+
+    void projectToY(boolean projectToY) {
+        if (projectToY && !ACTIVE_PROJECTORS.contains(yProjector)) {
+            ACTIVE_PROJECTORS.add(yProjector);
+        } else if (!projectToY) {
+            ACTIVE_PROJECTORS.remove(yProjector);
+        }
+    }
+
+    void projectToZ(boolean projectToZ) {
+        if (projectToZ && !ACTIVE_PROJECTORS.contains(zProjector)) {
+            ACTIVE_PROJECTORS.add(zProjector);
+        } else if (!projectToZ) {
+            ACTIVE_PROJECTORS.remove(zProjector);
+        }
+    }
+
+    private abstract class Projector {
+
+        float edgeCoord;
+
+        Projector() {
+            edgeCoord = Float.POSITIVE_INFINITY;
+        }
+
+        void setEdgeCoord(float edgeCoord) {
+            this.edgeCoord = edgeCoord;
+        }
+
+        abstract void prepareDrawing();
+
+        abstract void drawGLES2(Coord3d c);
+
+        abstract void drawGL2(Coord3d c, GL gl);
+
+    }
+
+    private class XProjector extends Projector {
+
+        private float[][] circleEdges;
+
+        public void prepareDrawing() {
+            float yWidth = (bbox.getYmax() - bbox.getYmin()) / 100 * scaleFactor;
+            float zWidth = (bbox.getZmax() - bbox.getZmin()) / 100 * scaleFactor;
+            circleEdges = new float[NUM_SEGMENTS + 1][2];
+            for (int i = 0; i <= NUM_SEGMENTS; i++) {
+                circleEdges[i][0] = yWidth * COSINES[i];
+                circleEdges[i][1] = zWidth * SINES[i];
+            }
+        }
+
+        @Override
+        public void drawGLES2(Coord3d c) {
+            GLES2CompatUtils.glBegin(GL.GL_TRIANGLE_FAN);
+            GLES2CompatUtils.glVertex3d(edgeCoord, c.y, c.z);
+            for (float[] circleEdge : circleEdges) {
+                GLES2CompatUtils.glVertex3d(edgeCoord, circleEdge[0] + c.y, circleEdge[1] + c.z);
+            }
+            GLES2CompatUtils.glEnd();
+        }
+
+        @Override
+        void drawGL2(Coord3d c, GL gl) {
+            gl.getGL2().glBegin(GL.GL_TRIANGLE_FAN);
+            gl.getGL2().glVertex3d(edgeCoord, c.y, c.z);
+            for (float[] circleEdge : circleEdges) {
+                gl.getGL2().glVertex3d(edgeCoord, circleEdge[0] + c.y, circleEdge[1] + c.z);
+            }
+            gl.getGL2().glEnd();
+        }
+    }
+
+    private class YProjector extends Projector {
+
+        private float[][] circleEdges;
+
+        public void prepareDrawing() {
+            float xWidth = (bbox.getXmax() - bbox.getXmin()) / 100 * scaleFactor;
+            float zWidth = (bbox.getZmax() - bbox.getZmin()) / 100 * scaleFactor;
+            circleEdges = new float[NUM_SEGMENTS + 1][2];
+            for (int i = 0; i <= NUM_SEGMENTS; i++) {
+                circleEdges[i][0] = xWidth * COSINES[i];
+                circleEdges[i][1] = zWidth * SINES[i];
+            }
+        }
+
+        @Override
+        public void drawGLES2(Coord3d c) {
+            GLES2CompatUtils.glBegin(GL.GL_TRIANGLE_FAN);
+            GLES2CompatUtils.glVertex3d(c.x, edgeCoord, c.z);
+            for (float[] circleEdge : circleEdges) {
+                GLES2CompatUtils.glVertex3d(circleEdge[0] + c.x, edgeCoord, circleEdge[1] + c.z);
+            }
+            GLES2CompatUtils.glEnd();
+        }
+
+        @Override
+        void drawGL2(Coord3d c, GL gl) {
+            gl.getGL2().glBegin(GL.GL_TRIANGLE_FAN);
+            gl.getGL2().glVertex3d(c.x, edgeCoord, c.z);
+            for (float[] circleEdge : circleEdges) {
+                gl.getGL2().glVertex3d(circleEdge[0] + c.x, edgeCoord, circleEdge[1] + c.z);
+            }
+            gl.getGL2().glEnd();
+        }
+    }
+
+    private class ZProjector extends Projector {
+
+        private float[][] circleEdges;
+
+        public void prepareDrawing() {
+            float xWidth = (bbox.getXmax() - bbox.getXmin()) / 100 * scaleFactor;
+            float yWidth = (bbox.getYmax() - bbox.getYmin()) / 100 * scaleFactor;
+            circleEdges = new float[NUM_SEGMENTS + 1][2];
+            for (int i = 0; i <= NUM_SEGMENTS; i++) {
+                circleEdges[i][0] = xWidth * COSINES[i];
+                circleEdges[i][1] = yWidth * SINES[i];
+            }
+        }
+
+        @Override
+        public void drawGLES2(Coord3d c) {
+            GLES2CompatUtils.glBegin(GL.GL_TRIANGLE_FAN);
+            GLES2CompatUtils.glVertex3d(c.x, c.y, edgeCoord);
+            for (float[] circleEdge : circleEdges) {
+                GLES2CompatUtils.glVertex3d(circleEdge[0] + c.x, circleEdge[1] + c.y, edgeCoord);
+            }
+            GLES2CompatUtils.glEnd();
+        }
+
+        @Override
+        void drawGL2(Coord3d c, GL gl) {
+            gl.getGL2().glBegin(GL.GL_TRIANGLE_FAN);
+            gl.getGL2().glVertex3d(c.x, c.y, edgeCoord);
+            for (float[] circleEdge : circleEdges) {
+                gl.getGL2().glVertex3d(circleEdge[0] + c.x, circleEdge[1] + c.y, edgeCoord);
+            }
+            gl.getGL2().glEnd();
+        }
+    }
+
+}
--- snap-rcp/src/main/java/org/esa/snap/rcp/statistics/ScatterPlot3dJzyPanel.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/statistics/ScatterPlot3dJzyPanel.java
@@ -18,6 +18,8 @@ import org.jzy3d.maths.IntegerCoord2d;
 import org.jzy3d.maths.Range;
 import org.jzy3d.maths.Scale;
 import org.jzy3d.picking.PickingSupport;
+import org.jzy3d.plot3d.primitives.LineStrip;
+import org.jzy3d.plot3d.primitives.Point;
 import org.jzy3d.plot3d.primitives.Scatter;
 import org.jzy3d.plot3d.primitives.pickable.Pickable;
 import org.jzy3d.plot3d.primitives.pickable.PickablePoint;
@@ -38,8 +40,6 @@ import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.jzy3d.plot3d.primitives.pickable.PickablePoint;
-
 /**
  * @author Tonio Fincke
  */
@@ -47,6 +47,7 @@ class ScatterPlot3dJzyPanel extends JPanel {
 
     private Chart chart;
     private Scatter scatter;
+    private ScatterPlot3DProjectionScatter projectionScatter;
     private JLabel titleLabel;
     private PickingSupport pickingSupport;
     private Range xRange;
@@ -54,12 +55,17 @@ class ScatterPlot3dJzyPanel extends JPanel {
     private Range zRange;
     private DrawableTextWrapper textWrapper;
     private ScatterPlot3dCameraMouseController cameraController;
+    private LineStrip xLine;
+    private LineStrip yLine;
+    private LineStrip zLine;
+    private Coord3d currentVertex;
+    private static final Color PROJECTION_COLOR = new Color(0.8f, 0.8f, 0.8f, 0.25f);
 
     void init() {
         chart = NewtChartComponentFactory.chart(Quality.Nicest, "newt");
         pickingSupport = new ScatterPlot3DPickingSupport();
         textWrapper = new DrawableTextWrapper("", new Coord3d(0, 0, 0),
-                                              Color.BLACK, new TextBitmapRenderer());
+                Color.BLACK, new TextBitmapRenderer());
         pickingSupport.addObjectPickedListener((vertices, picking) -> {
             for (Object vertex : vertices) {
                 final Coord3d vertexCoords = (Coord3d) vertex;
@@ -68,9 +74,30 @@ class ScatterPlot3dJzyPanel extends JPanel {
             }
             renderChart();
         });
+        xLine = new LineStrip();
+        xLine.setWireframeColor(new Color(0.8f, 0.0f, 0.0f));
+        xLine.setWidth(2.f);
+        chart.addDrawable(xLine);
+        yLine = new LineStrip();
+        yLine.setWireframeColor(new Color(0.8f, 0.0f, 0.0f));
+        yLine.setWidth(2.f);
+        chart.addDrawable(yLine);
+        zLine = new LineStrip();
+        zLine.setWireframeColor(new Color(0.8f, 0.0f, 0.0f));
+        zLine.setWidth(2.f);
+        chart.addDrawable(zLine);
+        pickingSupport.addObjectPickedListener((vertices, picking) -> {
+            for (Object vertex : vertices) {
+                currentVertex = (Coord3d) vertex;
+            }
+            renderChart();
+        });
         scatter = new Scatter();
         scatter.setWidth(10.0f);
         chart.getScene().add(scatter);
+        projectionScatter = new ScatterPlot3DProjectionScatter();
+        projectionScatter.setColor(PROJECTION_COLOR);
+        chart.getScene().add(projectionScatter);
         chart.add(textWrapper);
         setLayout(new BorderLayout());
         cameraController = new ScatterPlot3dCameraMouseController(this.chart, pickingSupport);
@@ -79,14 +106,57 @@ class ScatterPlot3dJzyPanel extends JPanel {
         add((Component) this.chart.getCanvas(), BorderLayout.CENTER);
     }
 
+    private void updateLine() {
+        xLine.clear();
+        yLine.clear();
+        zLine.clear();
+        if (currentVertex != null) {
+            Coord3d xVertex = new Coord3d(getXAxisCoord(), currentVertex.y, currentVertex.z);
+            Coord3d yVertex = new Coord3d(currentVertex.x, getYAxisCoord(), currentVertex.z);
+            Coord3d zVertex = new Coord3d(currentVertex.x, currentVertex.y, getZAxisCoord());
+            Point currentPoint = new Point(currentVertex);
+            xLine.add(currentPoint);
+            xLine.add(new Point(xVertex));
+            yLine.add(currentPoint);
+            yLine.add(new Point(yVertex));
+            zLine.add(currentPoint);
+            zLine.add(new Point(zVertex));
+        }
+    }
+
+    private float getXAxisCoord() {
+        View view = chart.getView();
+        if (Math.cos(view.getViewPoint().x) > 0) {
+            return view.getBounds().getXmin();
+        }
+        return view.getBounds().getXmax();
+    }
+
+    private float getYAxisCoord() {
+        View view = chart.getView();
+        if (Math.sin(view.getViewPoint().x) > 0) {
+            return view.getBounds().getYmin();
+        }
+        return view.getBounds().getYmax();
+    }
+
+    private float getZAxisCoord() {
+        View view = chart.getView();
+        if (view.getViewPoint().y > 0) {
+            return view.getBounds().getZmin();
+        }
+        return view.getBounds().getZmax();
+    }
+
     void setChartTitle(String chartTitle) {
         titleLabel.setText(chartTitle);
     }
 
     void setLabelNames(String xLabelName, String yLabelName, String zLabelName) {
-        chart.getAxeLayout().setXAxeLabel(xLabelName);
-        chart.getAxeLayout().setYAxeLabel(yLabelName);
-        chart.getAxeLayout().setZAxeLabel(zLabelName);
+        chart.getAxeLayout().setXAxeLabel("x =" + xLabelName);
+        chart.getAxeLayout().setYAxeLabel("y = " + yLabelName);
+        chart.getAxeLayout().setZAxeLabel("z = " + zLabelName);
+        chart.getAxeLayout().setFaceDisplayed(true);
     }
 
     void setChartData(float[] xData, float[] yData, float[] zData, double xScale, double yScale, double zScale) {
@@ -98,12 +168,30 @@ class ScatterPlot3dJzyPanel extends JPanel {
             pickingSupport.registerPickableObject(pickablePoint, points[i]);
         }
         scatter.setData(points);
+        projectionScatter.setData(points);
+        adjustProjectionScatterToBounds();
+    }
+
+    void projectToX(boolean projectToX) {
+        projectionScatter.projectToX(projectToX);
+    }
+
+    void projectToY(boolean projectToY) {
+        projectionScatter.projectToY(projectToY);
+    }
+
+    void projectToZ(boolean projectToZ) {
+        projectionScatter.projectToZ(projectToZ);
+    }
+
+    private void adjustProjectionScatterToBounds() {
+        projectionScatter.setEdgeCoords(getXAxisCoord(), getYAxisCoord(), getZAxisCoord());
     }
 
     void setColors(int[] colorData, int numColorBands) {
         Color[] colors = new Color[colorData.length / numColorBands];
         for (int i = 0; i < colorData.length / numColorBands; i++) {
-            colors[i] = new Color(colorData[numColorBands * i], colorData[ numColorBands * i + 1],
+            colors[i] = new Color(colorData[numColorBands * i], colorData[numColorBands * i + 1],
                     colorData[numColorBands * i + 2], 63);
         }
         scatter.setColors(colors);
@@ -115,10 +203,6 @@ class ScatterPlot3dJzyPanel extends JPanel {
         zRange = new Range(zMin, zMax);
         final BoundingBox3d box = new BoundingBox3d(xRange, yRange, zRange);
         chart.getView().setBoundManual(box);
-    }
-
-    void toggleAnimate() {
-        cameraController.toggleAnimate();
     }
 
     void renderChart() {
@@ -145,13 +229,15 @@ class ScatterPlot3dJzyPanel extends JPanel {
             if (isLeftDown(e)) {
                 Coord2d move = mouse.sub(prevMouse).div(100);
                 rotate(move);
+                updateLine();
+                adjustProjectionScatterToBounds();
             }
             // Shift
             else if (isRightDown(e)) {
                 final View view = chart.getView();
                 final Coord3d prevMouse3D = view.projectMouse((int) prevMouse.x, (int) prevMouse.y);
                 final Coord3d move3D = view.projectMouse((int) mouse.x, (int) mouse.y);
-                final Coord3d diff = prevMouse3D.sub(move3D).div(100);
+                final Coord3d diff = prevMouse3D.sub(move3D);
                 final Scale xScale = new Scale(view.getBounds().getXmin(), view.getBounds().getXmax());
                 final Scale yScale = new Scale(view.getBounds().getYmin(), view.getBounds().getYmax());
                 final Scale zScale = new Scale(view.getBounds().getZmin(), view.getBounds().getZmax());
@@ -164,6 +250,8 @@ class ScatterPlot3dJzyPanel extends JPanel {
                 view.setScaleX(newXScale);
                 view.setScaleY(newYScale);
                 view.setScaleZ(newZScale);
+                updateLine();
+                adjustProjectionScatterToBounds();
             }
 
             prevMouse = mouse;
@@ -174,35 +262,36 @@ class ScatterPlot3dJzyPanel extends JPanel {
             stopThreadController();
 
             float factor = NewtMouseUtilities.convertWheelRotation(e, 1.0f, 10.0f);
+            projectionScatter.scale(factor);
             zoomX(factor);
             zoomY(factor);
             zoomZ(factor);
+            updateLine();
+            adjustProjectionScatterToBounds();
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
-            int yflip = -y + targets.get(0).getCanvas().getRendererHeight();
-            prevMouse.x = x;
-            prevMouse.y = y;// yflip;
-            View view = targets.get(0).getView();
-            GL gl = chart().getView().getCurrentGL();
-//
-            // will trigger vertex selection event to those subscribing to
-            pickingSupport.pickObjects(gl, glu, view, null, new IntegerCoord2d(x, yflip));
-            chart().getView().getCurrentContext().release();
-        }
-
-        void toggleAnimate() {
-            if (this.threadController != null) {
+            if (e.getClickCount() == 2 && this.threadController != null) {
                 if (!isAnimating) {
                     this.threadController.start();
                 } else {
                     this.threadController.stop();
                 }
+                isAnimating = !isAnimating;
+            } else {
+                int x = e.getX();
+                int y = e.getY();
+                int yflip = -y + targets.get(0).getCanvas().getRendererHeight();
+                prevMouse.x = x;
+                prevMouse.y = y;// yflip;
+                View view = targets.get(0).getView();
+                GL gl = chart().getView().getCurrentGL();
+
+//                will trigger vertex selection event to those subscribing to
+                pickingSupport.pickObjects(gl, glu, view, null, new IntegerCoord2d(x, yflip));
+                chart().getView().getCurrentContext().release();
             }
-            isAnimating = !isAnimating;
         }
     }
 
@@ -269,9 +358,9 @@ class ScatterPlot3dJzyPanel extends JPanel {
             int minZ = Integer.MAX_VALUE;
             int pickableIndex = -1;
             for (int j = 0; j < hits; j++) {
-                if (selectBuf[4*j + 1] < minZ) {
-                    minZ = selectBuf[4*j + 1];
-                    pickableIndex = selectBuf[4*j + 3];
+                if (selectBuf[4 * j + 1] < minZ) {
+                    minZ = selectBuf[4 * j + 1];
+                    pickableIndex = selectBuf[4 * j + 3];
                 }
             }
             final Pickable pickable = pickables.get(pickableIndex);
