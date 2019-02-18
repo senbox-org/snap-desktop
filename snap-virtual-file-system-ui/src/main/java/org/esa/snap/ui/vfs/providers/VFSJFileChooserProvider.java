2466e7534f62629a92a374289a6682d193bedb19
Adrian DRAGHICI <adraghici@c-s.ro>
2019-02-18 19:18:09 +0200

    Add File Chooser Provider for VFS.

---
+++ snap-virtual-file-system-ui/src/main/java/org/esa/snap/ui/vfs/providers/VFSJFileChooserProvider.java
@@ -0,0 +1,34 @@
+package org.esa.snap.ui.vfs.providers;
+
+
+import org.esa.snap.ui.vfs.niojfilechooser.VFSJFileChooser;
+import org.esa.snap.core.vfs_spi.VFSProvider;
+
+import javax.swing.*;
+
+/**
+ * File Chooser Provider for VFS.
+ * VFS Service provider for access VFSJFileChooser methods from outside of snap-virtual-file-system-ui module via SPI.
+ *
+ * @author Adrian Drăghici
+ */
+public final class VFSJFileChooserProvider implements VFSProvider {
+
+    private static final String SERVICE_METHOD_GET_JFILE_CHOOSER = "getJFileChooser";
+
+    public VFSJFileChooserProvider() {
+    }
+
+    @Override
+    public <R> R runVFSService(String serviceMethodName, Class<? extends R> returnType, Object... params) {
+        switch (serviceMethodName) {
+            case SERVICE_METHOD_GET_JFILE_CHOOSER:
+                if (params.length == 1) {
+                    return returnType.cast(VFSJFileChooser.getJFileChooser((JFileChooser) params[0]));
+                }
+                break;
+        }
+        return null;
+    }
+
+}
