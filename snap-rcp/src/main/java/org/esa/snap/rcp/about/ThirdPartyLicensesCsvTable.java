2bb3e597c7550e0709c1ddbfbebcffae8fc0b4fb
marpet <marco.peters@brockmann-consult.de>
2019-04-16 15:47:06 +0200

    some enhancements to LicenseAboutBox

--- snap-rcp/src/main/java/org/esa/snap/rcp/about/ThirdPartyLicensesCsvTable.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/about/ThirdPartyLicensesCsvTable.java
@@ -2,7 +2,9 @@ package org.esa.snap.rcp.about;
 
 import org.esa.snap.core.gpf.OperatorException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -13,62 +15,37 @@ import java.util.StringTokenizer;
  * @author olafd
  */
 public class ThirdPartyLicensesCsvTable {
-    private static final String THIRD_PARTY_LICENSE_DEFAULT_FILE_NAME = "THIRDPARTY_LICENSES.txt";
 
     private List<String> name;
     private List<String> descrUse;
     private List<String> iprOwner;
-    private List<String> license;
-    private List<String> compatibleWithSnapGpl;
-    private List<String> comment;
     private List<String> iprOwnerUrl;
+    private List<String> license;
     private List<String> licenseUrl;
-    private List<String> commentUrl;
 
     private BufferedReader bufferedReader;
-    private InputStream inputStream;
 
     /**
      * Provides the licenses csv table from a resource file.
-     *
      */
-    ThirdPartyLicensesCsvTable() {
-        inputStream = getClass().getResourceAsStream(THIRD_PARTY_LICENSE_DEFAULT_FILE_NAME);
-        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        loadTable();
-    }
-
-    /**
-     * Provides the licenses csv table from an arbitrary file.
-     *
-     * @param filename - full path of arbitrary csv file
-     */
-    ThirdPartyLicensesCsvTable(String filename) {
-        try {
-            inputStream = new FileInputStream(filename);
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    ThirdPartyLicensesCsvTable(Reader licensesReader) {
+        bufferedReader = new BufferedReader(licensesReader);
         loadTable();
     }
 
     private void loadTable() {
-        // currently we have 9 columns in file:
+        // currently we have 6 columns in file:
         this.name = new ArrayList<>();
         this.descrUse = new ArrayList<>();
         this.iprOwner = new ArrayList<>();
         this.license = new ArrayList<>();
-        this.compatibleWithSnapGpl = new ArrayList<>();
-        this.comment = new ArrayList<>();
         this.iprOwnerUrl = new ArrayList<>();
         this.licenseUrl = new ArrayList<>();
-        this.commentUrl = new ArrayList<>();
 
-        readTableFromFile();
+        readTable();
     }
 
-    private void readTableFromFile() {
+    private void readTable() {
         StringTokenizer st;
         try {
             int i = 0;
@@ -91,31 +68,16 @@ public class ThirdPartyLicensesCsvTable {
                     setLicense(i, st.nextToken());
                 }
                 if (st.hasMoreTokens()) {
-                    setCompatibleWithSnapGpl(i, st.nextToken());
-                }
-                if (st.hasMoreTokens()) {
-                    setComment(i, st.nextToken());
-                }
-                if (st.hasMoreTokens()) {
                     setIprOwnerUrl(i, st.nextToken());
                 }
                 if (st.hasMoreTokens()) {
                     setLicenseUrl(i, st.nextToken());
                 }
-                if (st.hasMoreTokens()) {
-                    setCommentUrl(i, st.nextToken());
-                }
 
                 i++;
             }
         } catch (IOException | NumberFormatException e) {
-            throw new OperatorException("Failed to load SolarSpectrumTable: \n" + e.getMessage(), e);
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            throw new OperatorException("Failed to load 3rd-party license table: \n" + e.getMessage(), e);
         }
     }
 
@@ -126,6 +88,7 @@ public class ThirdPartyLicensesCsvTable {
     private void setName(int index, String name) {
         this.name.add(index, name);
     }
+
     public String getName(int index) {
         return name.get(index);
     }
@@ -133,6 +96,7 @@ public class ThirdPartyLicensesCsvTable {
     private void setDescrUse(int index, String descrUse) {
         this.descrUse.add(index, descrUse);
     }
+
     String getDescrUse(int index) {
         return descrUse.get(index);
     }
@@ -140,6 +104,7 @@ public class ThirdPartyLicensesCsvTable {
     private void setIprOwner(int index, String iprOwner) {
         this.iprOwner.add(index, iprOwner);
     }
+
     String getIprOwner(int index) {
         return iprOwner.get(index);
     }
@@ -147,27 +112,15 @@ public class ThirdPartyLicensesCsvTable {
     private void setLicense(int index, String license) {
         this.license.add(index, license);
     }
+
     String getLicense(int index) {
         return license.get(index);
-    }
-
-    private void setCompatibleWithSnapGpl(int index, String compatibleWithSnapGpl) {
-        this.compatibleWithSnapGpl.add(index, compatibleWithSnapGpl);
-    }
-    String getCompatibleWithSnapGpl(int index) {
-        return compatibleWithSnapGpl.get(index);
-    }
-
-    private void setComment(int index, String comment) {
-        this.comment.add(index, comment);
-    }
-    String getComment(int index) {
-        return comment.get(index);
     }
 
     private void setIprOwnerUrl(int index, String iprOwnerUrl) {
         this.iprOwnerUrl.add(index, iprOwnerUrl);
     }
+
     String getIprOwnerUrl(int index) {
         return iprOwnerUrl.get(index);
     }
@@ -175,15 +128,9 @@ public class ThirdPartyLicensesCsvTable {
     private void setLicenseUrl(int index, String licenseUrl) {
         this.licenseUrl.add(index, licenseUrl);
     }
+
     String getLicenseUrl(int index) {
         return licenseUrl.get(index);
-    }
-
-    private void setCommentUrl(int index, String commentUrl) {
-        this.commentUrl.add(index, commentUrl);
-    }
-    String getCommentUrl(int index) {
-        return commentUrl.get(index);
     }
 
 }
--- snap-rcp/src/test/java/org/esa/snap/rcp/about/ThirdPartyLicensesTest.java
+++ snap-rcp/src/test/java/org/esa/snap/rcp/about/ThirdPartyLicensesTest.java
@@ -2,6 +2,10 @@ package org.esa.snap.rcp.about;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -9,7 +13,8 @@ public class ThirdPartyLicensesTest {
 
     @Test
     public void testLoadThirdPartyLicensesFromCsv_fromResource() {
-        final ThirdPartyLicensesCsvTable licensesCsvTable = new ThirdPartyLicensesCsvTable();
+        InputStream is = ThirdPartyLicensesTest.class.getResourceAsStream("TEST_THIRDPARTY_LICENSES.txt");
+        final ThirdPartyLicensesCsvTable licensesCsvTable = new ThirdPartyLicensesCsvTable(new BufferedReader(new InputStreamReader(is)));
         assertNotNull(licensesCsvTable);
 
         assertEquals("Java SE JRE", licensesCsvTable.getName(0));
@@ -28,14 +33,6 @@ public class ThirdPartyLicensesTest {
         assertEquals("Apache License", licensesCsvTable.getLicense(62));
         assertEquals("MPL", licensesCsvTable.getLicense(112));
 
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(0));
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(62));
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(112));
-
-        assertEquals("Distribution FAQs", licensesCsvTable.getComment(0));
-        assertEquals("NONE", licensesCsvTable.getComment(62));
-        assertEquals("NONE", licensesCsvTable.getComment(112));
-
         assertEquals("http://www.oracle.com/technetwork/java/javase/overview/index.html", licensesCsvTable.getIprOwnerUrl(0));
         assertEquals("http://olingo.apache.org/", licensesCsvTable.getIprOwnerUrl(62));
         assertEquals("http://www.lowagie.com/iText/", licensesCsvTable.getIprOwnerUrl(112));
@@ -44,52 +41,6 @@ public class ThirdPartyLicensesTest {
         assertEquals("https://www.apache.org/licenses/", licensesCsvTable.getLicenseUrl(62));
         assertEquals("https://www.mozilla.org/en-US/MPL/", licensesCsvTable.getLicenseUrl(112));
 
-        assertEquals("https://java.com/en/download/faq/", licensesCsvTable.getCommentUrl(0));
-        assertEquals("NONE", licensesCsvTable.getCommentUrl(62));
-        assertEquals("NONE", licensesCsvTable.getCommentUrl(112));
     }
 
-    @Test
-    public void testLoadThirdPartyLicensesFromCsv_fromFile() {
-        final String licensesFileString = ThirdPartyLicensesCsvTable.class.getResource("THIRDPARTY_LICENSES.txt").getPath();
-
-        final ThirdPartyLicensesCsvTable licensesCsvTable = new ThirdPartyLicensesCsvTable(licensesFileString);
-        assertNotNull(licensesCsvTable);
-
-        assertEquals("Java SE JRE", licensesCsvTable.getName(0));
-        assertEquals("olingo", licensesCsvTable.getName(62));
-        assertEquals("iText", licensesCsvTable.getName(112));
-
-        assertEquals("Java Runtime", licensesCsvTable.getDescrUse(0));
-        assertEquals("Open Data Protocol", licensesCsvTable.getDescrUse(62));
-        assertEquals("PDF Library", licensesCsvTable.getDescrUse(112));
-
-        assertEquals("Oracle", licensesCsvTable.getIprOwner(0));
-        assertEquals("olingo", licensesCsvTable.getIprOwner(62));
-        assertEquals("lowagie", licensesCsvTable.getIprOwner(112));
-
-        assertEquals("Oracle Binary Code License", licensesCsvTable.getLicense(0));
-        assertEquals("Apache License", licensesCsvTable.getLicense(62));
-        assertEquals("MPL", licensesCsvTable.getLicense(112));
-
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(0));
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(62));
-        assertEquals("YES", licensesCsvTable.getCompatibleWithSnapGpl(112));
-
-        assertEquals("Distribution FAQs", licensesCsvTable.getComment(0));
-        assertEquals("NONE", licensesCsvTable.getComment(62));
-        assertEquals("NONE", licensesCsvTable.getComment(112));
-
-        assertEquals("http://www.oracle.com/technetwork/java/javase/overview/index.html", licensesCsvTable.getIprOwnerUrl(0));
-        assertEquals("http://olingo.apache.org/", licensesCsvTable.getIprOwnerUrl(62));
-        assertEquals("http://www.lowagie.com/iText/", licensesCsvTable.getIprOwnerUrl(112));
-
-        assertEquals("http://www.oracle.com/technetwork/java/javase/terms/license/index.html", licensesCsvTable.getLicenseUrl(0));
-        assertEquals("https://www.apache.org/licenses/", licensesCsvTable.getLicenseUrl(62));
-        assertEquals("https://www.mozilla.org/en-US/MPL/", licensesCsvTable.getLicenseUrl(112));
-
-        assertEquals("https://java.com/en/download/faq/", licensesCsvTable.getCommentUrl(0));
-        assertEquals("NONE", licensesCsvTable.getCommentUrl(62));
-        assertEquals("NONE", licensesCsvTable.getCommentUrl(112));
-    }
 }
