6140c0e19c1bcbe2301eb8f583fce9042f61ad38
marpet <marco.peters@brockmann-consult.de>
2019-07-08 18:14:03 +0200

    perform check only if application window is visible
    updated text

--- snap-rcp/src/main/java/org/esa/snap/rcp/util/DesktopVersionCheck.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/util/DesktopVersionCheck.java
@@ -2,6 +2,7 @@ package org.esa.snap.rcp.util;
 
 import org.esa.snap.core.util.SystemUtils;
 import org.esa.snap.core.util.VersionChecker;
+import org.esa.snap.rcp.SnapApp;
 import org.openide.awt.CheckForUpdatesProvider;
 import org.openide.modules.OnStop;
 import org.openide.util.Lookup;
@@ -12,7 +13,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.Cursor;
-import java.time.LocalDateTime;
 
 /**
  * @author Marco Peters
@@ -23,8 +23,8 @@ public class DesktopVersionCheck {
     private static final String STEP_WEB_PAGE = SystemUtils.getApplicationHomepageUrl();
     private static final String MSG_UPDATE_INFO =
             "<html>A new SNAP version is available for download!<br>" +
-            "Currently installed %s, available is %s.<br>" +
-            "Please visit the SNAP home page at";
+                    "Currently installed %s, available is %s.<br>" +
+                    "Please visit the SNAP home page at";
     private static final VersionChecker VERSION_CHECKER = VersionChecker.getInstance();
 
     private static boolean hasChecked = false;
@@ -37,7 +37,11 @@ public class DesktopVersionCheck {
 
         @Override
         public void run() {
-
+            // @OnShowing is triggered if Utilities.actionsGlobalContext() is called. This happens in some tests (which use actions).
+            // So we need to check if the Desktop is really running. If not, we can skip the version check.
+            if (!SnapApp.getDefault().getAppContext().getApplicationWindow().isVisible()) {
+                return;
+            }
             hasChecked = true;
             if (VERSION_CHECKER.checkForNewRelease()) {
                 final JPanel panel = new JPanel();
@@ -55,16 +59,15 @@ public class DesktopVersionCheck {
                 JOptionPane.showMessageDialog(null, panel);
             } else {
                 final String message =
-                        "Snap is up to date.\n" +
-                        "Press 'Yes' if you want to check for existing plugin updates too.\n\n";
-                Dialogs.Answer decision = Dialogs.requestDecision("Update Plugins", message, false, "optional.version.check.onstartup");
-                if(Dialogs.Answer.YES.equals(decision)) {
-                    // todo - use the follwoing code which is taken from org.netbeans.modules.autoupdate.ui.actions.CheckForUpdatesAction
+                        "You are running the latest major version " + VERSION_CHECKER.getLocalVersion() + " of SNAP.\n" +
+                                "Please check regularly for new plugin updates (Help -> Check for Updates...) \n" +
+                                "to get the best SNAP experience.\n\n" +
+                                "Press 'Yes', if you want to check for plugin updates now.\n\n";
+                Dialogs.Answer decision = Dialogs.requestDecision("SNAP Update", message, false, "optional.version.check.onstartup");
+                if (Dialogs.Answer.YES.equals(decision)) {
                     final CheckForUpdatesProvider checkForUpdatesProvider = Lookup.getDefault().lookup(CheckForUpdatesProvider.class);
                     assert checkForUpdatesProvider != null : "An instance of CheckForUpdatesProvider found in Lookup: " + Lookup.getDefault();
-                    if (checkForUpdatesProvider != null) {
-                        checkForUpdatesProvider.openCheckForUpdatesWizard(true);
-                    }
+                    checkForUpdatesProvider.openCheckForUpdatesWizard(true);
                 }
             }
         }
