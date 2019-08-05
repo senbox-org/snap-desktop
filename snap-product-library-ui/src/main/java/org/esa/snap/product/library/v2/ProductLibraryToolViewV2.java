f53979b98ad59f9ce253fa5fc27fe88333162889
jeancoravu <jean.coravu@c-s.ro>
2019-08-05 15:10:13 +0300

    Create new package 'org.esa.snap.product.library.v2' to store the new classes of the product library option.

--- snap-product-library-ui/src/main/java/org/esa/snap/product/library/v2/ProductLibraryToolViewV2.java
+++ snap-product-library-ui/src/main/java/org/esa/snap/product/library/v2/ProductLibraryToolViewV2.java
@@ -15,12 +15,35 @@
  */
 package org.esa.snap.product.library.v2;
 
+import org.esa.snap.productlibrary.rcp.toolviews.ProductLibraryToolView;
+import org.esa.snap.productlibrary.rcp.toolviews.model.repositories.RepositoryInterface;
 import org.esa.snap.rcp.windows.ToolTopComponent;
+import org.esa.snap.ui.UIUtils;
+import org.esa.snap.ui.loading.LabelListCellRenderer;
+import org.esa.snap.ui.loading.SwingUtils;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
 
 @TopComponent.Description(
         preferredID = "ProductLibraryTopComponentV2",
@@ -47,9 +70,100 @@ import org.openide.windows.TopComponent;
 })
 public class ProductLibraryToolViewV2 extends ToolTopComponent {
 
+    private boolean initialized;
+    private JComboBox<String> repositoryListComboBox;
+    private JTextField productNameTextField;
+
     public ProductLibraryToolViewV2() {
+        this.initialized = false;
+
+        setLayout(new BorderLayout());
     }
 
+    @Override
     protected void componentShowing() {
+        if (!this.initialized) {
+            this.initialized = true;
+
+            Insets defaultTextFieldMargins = buildDefaultTextFieldMargins();
+            Insets defaultListItemMargins = buildDefaultListItemMargins();
+
+            this.productNameTextField = new JTextField();
+            this.productNameTextField.setMargin(defaultTextFieldMargins);
+
+            int textFieldPreferredHeight = this.productNameTextField.getPreferredSize().height;
+
+            String[] availableDataSources = new String[] {"Scientific Hub", "Local data folder"};
+            this.repositoryListComboBox = new JComboBox<String>(availableDataSources);
+//            Dimension formatNameComboBoxSize = productFormatNameComboBox.getPreferredSize();
+//            formatNameComboBoxSize.height = textFieldPreferredHeight;
+//            productFormatNameComboBox.setPreferredSize(formatNameComboBoxSize);
+//            productFormatNameComboBox.setMinimumSize(formatNameComboBoxSize);
+//            LabelListCellRenderer<String> renderer = new LabelListCellRenderer<String>(defaultListItemMargins) {
+//                @Override
+//                protected String getItemDisplayText(String value) {
+//                    return value;
+//                }
+//            };
+            this.repositoryListComboBox.setMaximumRowCount(5);
+//            this.repositoryListComboBox.setRenderer(renderer);
+            this.repositoryListComboBox.setBackground(new Color(0, 0, 0, 0)); // set the transparent color
+            this.repositoryListComboBox.setOpaque(true);
+
+            Dimension buttonSize = new Dimension(textFieldPreferredHeight, textFieldPreferredHeight);
+
+            JPanel verticalButtonsPanel = new JPanel();
+            verticalButtonsPanel.setLayout(new BoxLayout(verticalButtonsPanel, BoxLayout.Y_AXIS));
+
+            ActionListener searchButtonListener = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            };
+
+            JButton searchButton = buildButton("/org/esa/snap/productlibrary/icons/search24.png", searchButtonListener, buttonSize);
+            JButton helpButton = buildButton("/org/esa/snap/resources/images/icons/Help24.gif", searchButtonListener, buttonSize);
+
+            JPanel headerPanel = new JPanel(new GridBagLayout());
+
+            int gapBetweenRows = 5;
+            int gapBetweenColumns = 5;
+            GridBagConstraints c = SwingUtils.buildConstraints(0, 0, GridBagConstraints.NONE, GridBagConstraints.WEST, 1, 1, 0, 0);
+            headerPanel.add(new JLabel("Data source"), c);
+
+            c = SwingUtils.buildConstraints(1, 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST, 1, 1, 0, gapBetweenColumns);
+            headerPanel.add(this.repositoryListComboBox, c);
+
+            c = SwingUtils.buildConstraints(2, 0, GridBagConstraints.NONE, GridBagConstraints.WEST, 1, 1, 0, gapBetweenColumns);
+            headerPanel.add(searchButton, c);
+
+            c = SwingUtils.buildConstraints(3, 0, GridBagConstraints.NONE, GridBagConstraints.WEST, 1, 1, 0, gapBetweenColumns);
+            headerPanel.add(helpButton, c);
+
+            add(headerPanel, BorderLayout.NORTH);
+            setBorder(new EmptyBorder(5, 5, 5, 5));
+        }
+    }
+
+    private Insets buildDefaultTextFieldMargins() {
+        return new Insets(3, 2, 3, 2);
+    }
+
+    private Insets buildDefaultListItemMargins() {
+        return new Insets(3, 2, 3, 2);
+    }
+
+    private static JButton buildButton(String resourceImagePath, ActionListener buttonListener, Dimension buttonSize) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL imageURL = classLoader.getResource(resourceImagePath);
+        ImageIcon icon = new ImageIcon(imageURL);
+        JButton button = new JButton(icon);
+        button.setFocusable(false);
+        button.addActionListener(buttonListener);
+        button.setPreferredSize(buttonSize);
+        button.setMinimumSize(buttonSize);
+        button.setMaximumSize(buttonSize);
+        return button;
     }
 }
