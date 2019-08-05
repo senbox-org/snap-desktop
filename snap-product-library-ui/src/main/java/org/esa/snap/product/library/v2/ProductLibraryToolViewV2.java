e7df53a589aabfa8664849599ff37b02369f8fba
jeancoravu <jean.coravu@c-s.ro>
2019-08-05 11:14:17 +0300

    Create new package 'org.esa.snap.product.library.v2' to store the new classes of the product library option.

---
+++ snap-product-library-ui/src/main/java/org/esa/snap/product/library/v2/ProductLibraryToolViewV2.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 by Array Systems Computing Inc. http://www.array.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+package org.esa.snap.product.library.v2;
+
+import org.esa.snap.rcp.windows.ToolTopComponent;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
+
+@TopComponent.Description(
+        preferredID = "ProductLibraryTopComponentV2",
+        iconBase = "org/esa/snap/productlibrary/icons/search.png",
+        persistenceType = TopComponent.PERSISTENCE_ALWAYS
+)
+@TopComponent.Registration(
+        mode = "rightSlidingSide",
+        openAtStartup = true,
+        position = 0
+)
+@ActionID(category = "Window", id = "org.esa.snap.product.library.v2.ProductLibraryToolViewV2")
+@ActionReferences({
+        @ActionReference(path = "Menu/View/Tool Windows"),
+        @ActionReference(path = "Menu/File", position = 17)
+})
+@TopComponent.OpenActionRegistration(
+        displayName = "#CTL_ProductLibraryTopComponentV2Name",
+        preferredID = "ProductLibraryTopComponentV2"
+)
+@NbBundle.Messages({
+        "CTL_ProductLibraryTopComponentV2Name=Product Library V2",
+        "CTL_ProductLibraryTopComponentV2Description=Product Library V2",
+})
+public class ProductLibraryToolViewV2 extends ToolTopComponent {
+
+    public ProductLibraryToolViewV2() {
+    }
+
+    protected void componentShowing() {
+    }
+}
