8e4af24f9bb00fad787c8d54fb47cc63f90063ee
jeancoravu <jean.coravu@c-s.ro>
2019-09-06 09:26:59 +0300

    Read the product from the local database.

---
+++ snap-product-library-ui/src/main/java/org/esa/snap/product/library/ui/v2/repository/local/LoadProductListTimerRunnable.java
@@ -0,0 +1,56 @@
+package org.esa.snap.product.library.ui.v2.repository.local;
+
+import org.esa.snap.product.library.ui.v2.RemoteRepositoryProductListPanel;
+import org.esa.snap.product.library.ui.v2.ThreadListener;
+import org.esa.snap.product.library.ui.v2.thread.AbstractProgressTimerRunnable;
+import org.esa.snap.product.library.ui.v2.thread.ProgressPanel;
+import org.esa.snap.product.library.v2.database.DerbyDAL;
+import org.esa.snap.remote.products.repository.RepositoryProduct;
+
+import java.util.List;
+
+/**
+ * Created by jcoravu on 5/9/2019.
+ */
+public class LoadProductListTimerRunnable extends AbstractProgressTimerRunnable<List<RepositoryProduct>> {
+
+    private final ThreadListener threadListener;
+    private final RemoteRepositoryProductListPanel repositoryProductListPanel;
+
+    public LoadProductListTimerRunnable(ProgressPanel progressPanel, int threadId, ThreadListener threadListener,
+                                        RemoteRepositoryProductListPanel repositoryProductListPanel) {
+
+        super(progressPanel, threadId, 500);
+
+        this.threadListener = threadListener;
+        this.repositoryProductListPanel = repositoryProductListPanel;
+    }
+
+    @Override
+    protected List<RepositoryProduct> execute() throws Exception {
+        return DerbyDAL.loadProductList();
+    }
+
+    @Override
+    protected String getExceptionLoggingMessage() {
+        return "Failed to read the product list from the database.";
+    }
+
+    @Override
+    protected void onStopExecuting() {
+        this.threadListener.onStopExecuting(null);
+    }
+
+    @Override
+    protected void onSuccessfullyFinish(List<RepositoryProduct> results) {
+        this.repositoryProductListPanel.setProducts(results);
+        if (results.size() == 0) {
+            onShowInformationMessageDialog(this.repositoryProductListPanel, "No product available according to the filter values.", "Information");
+        }
+    }
+
+    @Override
+    protected void onFailed(Exception exception) {
+        onShowErrorMessageDialog(this.repositoryProductListPanel, "Failed to read the product list from the database.", "Error");
+    }
+}
