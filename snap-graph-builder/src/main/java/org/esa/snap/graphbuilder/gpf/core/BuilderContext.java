75d75c316214bc96628c516614ff6bf2f64f1a86
Mandarancio <manda.mgf@gmail.com>
2020-01-06 16:27:36 +0100

    initial work

---
+++ snap-graph-builder/src/main/java/org/esa/snap/graphbuilder/gpf/core/BuilderContext.java
@@ -0,0 +1,12 @@
+package org.esa.snap.graphbuilder.gpf.core;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.esa.snap.core.gpf.graph.Node;
+import org.esa.snap.core.gpf.graph.Graph;
+
+public class BuilderContext {
+    private ArrayList<Graph> graphs;
+    public BuilderContext(){}
+}
