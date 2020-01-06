1fae2a2500b4365484d750c5e37ac0fbc204704a
Mandarancio <manda.mgf@gmail.com>
2020-01-06 13:21:48 +0100

    Added new utility to compute node inter-dipedency

---
+++ snap-graph-builder/src/main/java/org/esa/snap/graphbuilder/gpf/core/Utils.java
@@ -0,0 +1,40 @@
+package org.esa.snap.graphbuilder.gpf.core;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.esa.snap.core.gpf.graph.Node;
+import org.esa.snap.core.gpf.graph.Graph;
+
+public class Utils {
+    public static List<Node> affectedNodes(Graph graph, Node node) {
+        ArrayList<Node> affected = new ArrayList<List>([node]);
+        Boolean changed = true;
+        while (changed) {
+            changed = false;
+            for (int i = 0; i < graph->getNodeCount(); i++) {
+                Node tmp = graph->getNodeCount(i);
+                if (!affected.contains(tmp)){
+                    for (NodeSource source: tmp.getSources()) {
+                        if (affected.contains(source.getSourceNode())) {
+                            affected.add(tmp);
+                            changed = true;
+                        }
+                    }
+                }
+            }
+        }
+        affected.remove(node);
+        return affected;
+    }
+
+    public static List<Node> dependencyNodes(Node node) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        for (NodeSource source: node.getSources()) {
+            Node m = source.getSourceNode();
+            nodes.add(m);
+            nodes.addAll(dependencyNodes(m))
+        }
+        return nodes;
+    }
+}
