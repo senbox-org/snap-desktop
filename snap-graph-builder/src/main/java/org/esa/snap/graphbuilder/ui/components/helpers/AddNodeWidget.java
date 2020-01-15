4d735255c4b3e74605a01f800f794636cd539919
Martino Ferrari <manda.mgf@gmail.com>
2020-01-15 01:45:32 +0100

    removed useless import

--- snap-graph-builder/src/main/java/org/esa/snap/graphbuilder/ui/components/helpers/AddNodeWidget.java
+++ snap-graph-builder/src/main/java/org/esa/snap/graphbuilder/ui/components/helpers/AddNodeWidget.java
@@ -4,7 +4,6 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.Shape;
 import java.awt.Stroke;
 import java.util.HashSet;
 import java.util.Set;
