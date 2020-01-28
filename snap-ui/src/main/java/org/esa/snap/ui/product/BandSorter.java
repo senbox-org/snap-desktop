6884d4a3e42847b798cdd4bd2fe07cc4af0c84a6
TomBlock <tom.block@brockmann-consult.de>
2020-01-28 13:29:18 +0100

    corrected BandSorter compare algorithm - old one was not compatible with Java 8 contract conditions

--- snap-ui/src/main/java/org/esa/snap/ui/product/BandSorter.java
+++ snap-ui/src/main/java/org/esa/snap/ui/product/BandSorter.java
@@ -17,6 +17,7 @@
 package org.esa.snap.ui.product;
 
 import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.ProductNode;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -27,41 +28,10 @@ import java.util.Comparator;
 class BandSorter {
 
     static Comparator<Band> createComparator() {
-        return new Comparator<Band>() {
-            @Override
-            public int compare(Band band1, Band band2) {
-                String band1Name = removeDigits(band1);
-                String band2Name = removeDigits(band2);
-                float wavelength1 = band1.getSpectralWavelength();
-                float wavelength2 = band2.getSpectralWavelength();
-
-                if (band1Name.equals(band2Name)) {
-                    // names without digits are equal: decide by wavelength
-                    if (wavelength1 > 0 && wavelength2 > 0 && wavelength1 != wavelength2) {
-                        return (int) (wavelength1 - wavelength2);
-                    } else {
-                        // wavelengths are equal: decide by original name
-                        return band1.getName().compareTo(band2.getName());
-                    }
-                } else {
-                    // names are not equal -> try to sort by wavelength
-                    if (wavelength1 > 0.0 && wavelength2 > 0.0) {
-                        return (int) (wavelength1 - wavelength2);
-                    } else {
-                        // names are not equal and there are no wavelengths: sort by name
-                        return band1.getName().compareTo(band2.getName());
-                    }
-                }
-            }
-        };
+        return Comparator.comparing(ProductNode::getName);
     }
 
     static void sort(Band[] allBands) {
-        Arrays.sort(allBands, createComparator());
+        Arrays.parallelSort(allBands, createComparator());
     }
-
-    private static String removeDigits(Band band) {
-        return band.getName().replaceAll("\\d", "");
-    }
-
 }
