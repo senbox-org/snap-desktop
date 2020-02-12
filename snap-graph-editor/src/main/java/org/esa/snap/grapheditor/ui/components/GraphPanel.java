26761a9929ab7a6eee13b19e7ae2f98ee7401635
Martino Ferrari <manda.mgf@gmail.com>
2020-02-12 23:18:58 +0100

    minor cleanup

--- snap-graph-editor/src/main/java/org/esa/snap/grapheditor/ui/components/GraphPanel.java
+++ snap-graph-editor/src/main/java/org/esa/snap/grapheditor/ui/components/GraphPanel.java
@@ -10,8 +10,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 
 import javax.swing.*;
 
