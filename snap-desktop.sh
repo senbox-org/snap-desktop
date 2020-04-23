29bdc45df62245095797665c8ba22c4081fd0c25
Martino Ferrari <manda.mgf@gmail.com>
2020-04-23 17:46:23 +0200

    added s1tbx kit to launcher

--- snap-desktop.sh
+++ snap-desktop.sh
@@ -5,7 +5,7 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JAVA=/usr/lib/jvm/java-1.8.0-zulu-amd64/bin/java
 MAIN_CLASS=org.esa.snap.nbexec.Launcher
 VM_ARGS="-Dsun.java2d.noddraw=true -Dsun.awt.nopixfmt=true -Dsun.java2d.dpiaware=false -Dorg.netbeans.level=INFO -Dsnap.debug=true -Xmx4G"
-PROGRAM_ARGS="--userdir $CWD/snap-application/target/userdir --clusters $CWD/../s2tbx/s2tbx-kit/target/netbeans_clusters/s2tbx --patches $CWD/../snap-engine/$/target/classes:$CWD/../s2tbx/$/target/classes"
+PROGRAM_ARGS="--userdir $CWD/snap-application/target/userdir --clusters $CWD/../s1tbx/s1tbx-kit/target/netbeans_clusters/s1tbx:$CWD/../s2tbx/s2tbx-kit/target/netbeans_clusters/s2tbx --patches $CWD/../snap-engine/$/target/classes:$CWD/../s1tbx/$/target/classes:$CWD/../s2tbx/$/target/classes"
 WORKING_DIR=$CWD/snap-application/target/snap
 CLASSPATH=$CWD/nbexec/target/classes
 
