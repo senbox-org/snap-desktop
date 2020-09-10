20bdb9e40206a871b90c36f65f5418c632cf9acd
oanahogoiu <oana.hogoiu@c-s.ro>
2020-09-10 12:04:46 +0300

    disable old Product Library & AOI Monitoring Help (even from search inside help)

--- snap-product-library-ui/src/main/java/org/esa/snap/productlibrary/docs/package-info.java
+++ snap-product-library-ui/src/main/java/org/esa/snap/productlibrary/docs/package-info.java
@@ -1,4 +1,5 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 1100)
+//DISABLE old Product Library & AOI Monitoring GUI & Help
+//@HelpSetRegistration(helpSet = "help.hs", position = 1100)
 package org.esa.snap.productlibrary.docs;
 
-import org.netbeans.api.javahelp.HelpSetRegistration;
+//import org.netbeans.api.javahelp.HelpSetRegistration;
