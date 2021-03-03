81552ef739aa44eaba451fa65993ddfe9f0c865b
tom <tom.block@brockmann-consult.de>
2021-03-03 08:46:33 +0100

    attached interpolator property to dialog

--- snap-rcp/src/main/java/org/esa/snap/rcp/preferences/general/GeoLocationController.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/preferences/general/GeoLocationController.java
@@ -38,6 +38,7 @@ import java.awt.Insets;
 
 import static com.bc.ceres.swing.TableLayout.Anchor;
 import static com.bc.ceres.swing.TableLayout.Fill;
+import static org.esa.snap.core.dataio.geocoding.util.InterpolatorFactory.SYSPROP_INTERPOLATOR_GEODETIC;
 import static org.esa.snap.rcp.pixelinfo.PixelInfoView.PREFERENCE_DEFAULT_SHOW_GEO_POS_DECIMALS;
 import static org.esa.snap.rcp.pixelinfo.PixelInfoView.PREFERENCE_DEFAULT_SHOW_PIXEL_POS_DECIMALS;
 import static org.esa.snap.rcp.pixelinfo.PixelInfoView.PREFERENCE_DEFAULT_SHOW_PIXEL_POS_OFFSET_1;
@@ -83,6 +84,7 @@ public final class GeoLocationController extends DefaultConfigController {
         final PropertySet propertySet = context.getPropertySet();
         final Property snapToExactGeolocationProperty = propertySet.getProperty(PREFERENCE_KEY_ADJUST_PIN_GEO_POS);
         final Property pixelGeocodingFractionAccuracyProperty = propertySet.getProperty(PREFERENCE_KEY_PIXEL_GEO_CODING_FRACTION_ACCURACY);
+        final Property pixelGeoCodingFractionInterpolatorProperty = propertySet.getProperty(SYSPROP_INTERPOLATOR_GEODETIC);
         final Property tiePointInverseHighPrecisionProperty = propertySet.getProperty(PREFERENCE_KEY_TIE_POINT_INVERSE_HIGH_PRECISION);
         final Property showGeoPosAsDecimals = propertySet.getProperty(PREFERENCE_KEY_SHOW_GEO_POS_DECIMALS);
         final Property showPixelPosAsDecimals = propertySet.getProperty(PREFERENCE_KEY_SHOW_PIXEL_POS_DECIMALS);
@@ -93,6 +95,9 @@ public final class GeoLocationController extends DefaultConfigController {
 
         descriptor = pixelGeocodingFractionAccuracyProperty.getDescriptor();
         final JComponent[] pixelGeocodingfractionAccuracyComponents = registry.findPropertyEditor(descriptor).createComponents(descriptor, context);
+
+        descriptor = pixelGeoCodingFractionInterpolatorProperty.getDescriptor();
+        final JComponent[] pixelGeocodingFractionInterpolatorComponents = registry.findPropertyEditor(descriptor).createComponents(descriptor, context);
 
         descriptor = tiePointInverseHighPrecisionProperty.getDescriptor();
         JComponent[] tiePointInverseHighPrecisionComponents = registry.findPropertyEditor(descriptor).createComponents(descriptor, context);
@@ -119,6 +124,7 @@ public final class GeoLocationController extends DefaultConfigController {
         pageUI.add(PreferenceUtils.createTitleLabel("General Settings"));
         pageUI.add(snapToExactGeolocationComponents[0]);
         pageUI.add(pixelGeocodingfractionAccuracyComponents[0]);
+        pageUI.add(pixelGeocodingFractionInterpolatorComponents[0]);
         pageUI.add(tiePointInverseHighPrecisionComponents[0]);
         tableLayout.createHorizontalSpacer();
 
@@ -146,6 +152,10 @@ public final class GeoLocationController extends DefaultConfigController {
         @Preference(label = "Use sub-pixel fraction accuracy for pixel-based geo-coding",
                 key = PREFERENCE_KEY_PIXEL_GEO_CODING_FRACTION_ACCURACY, config = "snap")
         boolean getPixelPosWithFractionAccuracy = false;
+
+        @Preference(label = "Use geodetic interpolation for sub-pixel fraction accuracy for pixel-based geo-coding",
+                key = SYSPROP_INTERPOLATOR_GEODETIC, config = "snap")
+        boolean useGeodeticInterpolator = false;
 
         @Preference(label = "Use high precision approximations for inverse tie point geo-coding",
                 key = PREFERENCE_KEY_TIE_POINT_INVERSE_HIGH_PRECISION, config = "snap")
