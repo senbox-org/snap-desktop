57e1c22b6944a211f6d8e0230661921e5a528ea6
Marco Peters <marco.peters@brockmann-consult.de>
2023-05-03 16:21:19 +0200

    org-esa-snap-ui-tooladapter-docs and org-esa-snap-ui-vfs-docs had same position 2.480. Now set org-esa-snap-ui-vfs-docs to 2481

--- snap-virtual-file-system-preferences-ui/src/main/java/org/esa/snap/ui/vfs/docs/package-info.java
+++ snap-virtual-file-system-preferences-ui/src/main/java/org/esa/snap/ui/vfs/docs/package-info.java
@@ -1,4 +1,4 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 2480)
+@HelpSetRegistration(helpSet = "help.hs", position = 2481)
 package org.esa.snap.ui.vfs.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
