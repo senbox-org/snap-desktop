f23849c1b63f4f0ffa936343233e25c8b0a824aa
Tom <tom.block@brockmann-consult.de>
2024-02-22 16:18:46 +0100

    fixed SNAP-3586

--- snap-rcp/src/main/java/org/esa/snap/rcp/spectrum/SpectraExportAction.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/spectrum/SpectraExportAction.java
@@ -19,27 +19,25 @@ package org.esa.snap.rcp.spectrum;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.Placemark;
 import org.esa.snap.core.util.PreferencesPropertyMap;
+import org.esa.snap.core.util.PropertyMap;
 import org.esa.snap.core.util.io.SnapFileFilter;
 import org.esa.snap.rcp.SnapApp;
 import org.esa.snap.ui.diagram.DiagramGraph;
 import org.esa.snap.ui.diagram.DiagramGraphIO;
 import org.esa.snap.ui.product.spectrum.DisplayableSpectrum;
 
-import javax.swing.AbstractAction;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.prefs.Preferences;
 
-//import org.esa.snap.visat.VisatApp;
-
 class SpectraExportAction extends AbstractAction {
 
-    private SpectrumTopComponent spectrumTopComponent;
+    private final SpectrumTopComponent spectrumTopComponent;
 
-    public SpectraExportAction(SpectrumTopComponent spectrumTopComponent) {
+    SpectraExportAction(SpectrumTopComponent spectrumTopComponent) {
         super("exportSpectra");
         this.spectrumTopComponent = spectrumTopComponent;
     }
@@ -49,28 +47,42 @@ class SpectraExportAction extends AbstractAction {
         exportSpectra();
     }
 
-
     private void exportSpectra() {
         final List<DisplayableSpectrum> selectedSpectra = spectrumTopComponent.getSelectedSpectra();
-        Placemark[] pins = spectrumTopComponent.getDisplayedPins();
-        Map<Placemark, Map<Band, Double>> energiesMap = spectrumTopComponent.getPinToEnergies();
+        final Placemark[] pins = spectrumTopComponent.getDisplayedPins();
+
         final List<SpectrumGraph> spectrumGraphList = new ArrayList<SpectrumGraph>();
+        final Map<Placemark, Map<Band, Double>> energiesMap = spectrumTopComponent.getPinToEnergies();
+
         for (Placemark pin : pins) {
+            Map<Band, Double> bandEnergyMap = energiesMap.get(pin);
             for (DisplayableSpectrum spectrumInDisplay : selectedSpectra) {
-                final SpectrumGraph spectrumGraph = new SpectrumGraph(pin, spectrumInDisplay.getSelectedBands());
-                spectrumGraph.readValues();
+                final Band[] selectedBands = spectrumInDisplay.getSelectedBands();
+                final double[] energies = getEnergies(selectedBands, bandEnergyMap);
+
+                final SpectrumGraph spectrumGraph = new SpectrumGraph(pin, selectedBands);
+                spectrumGraph.setEnergies(energies);
                 spectrumGraphList.add(spectrumGraph);
             }
         }
+
         DiagramGraph[] pinGraphs = spectrumGraphList.toArray(new DiagramGraph[0]);
 
         final Preferences preferences = SnapApp.getDefault().getPreferences();
-        final PreferencesPropertyMap preferencesPropertyMap = new PreferencesPropertyMap(preferences);
+        final PropertyMap preferencesPropertyMap = new PreferencesPropertyMap(preferences);
         DiagramGraphIO.writeGraphs(SwingUtilities.getWindowAncestor(spectrumTopComponent),
-                                   "Export Pin Spectra",
-                                   new SnapFileFilter[]{DiagramGraphIO.SPECTRA_CSV_FILE_FILTER},
-                                   preferencesPropertyMap,
-                                   pinGraphs);
+                "Export Pin Spectra",
+                new SnapFileFilter[]{DiagramGraphIO.SPECTRA_CSV_FILE_FILTER},
+                preferencesPropertyMap,
+                pinGraphs);
     }
 
+    // package access for testing only tb 2024-02-22
+    static double[] getEnergies(Band[] selectedBands, Map<Band, Double> bandDoubleMap) {
+        final double[] energies = new double[selectedBands.length];
+        for (int i = 0; i < selectedBands.length; i++) {
+            energies[i] = bandDoubleMap.get(selectedBands[i]);
+        }
+        return energies;
+    }
 }
