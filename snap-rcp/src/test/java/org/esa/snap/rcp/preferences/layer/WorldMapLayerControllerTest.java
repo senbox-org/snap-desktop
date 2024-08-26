ff5ba6c7ac879b65ba0bb5aa03b49676dca3940c
Tom <tom.block@brockmann-consult.de>
2024-08-26 13:37:58 +0200

    merged SNAP-3762 solution from 11.x

---
+++ snap-rcp/src/test/java/org/esa/snap/rcp/preferences/layer/WorldMapLayerControllerTest.java
@@ -0,0 +1,39 @@
+package org.esa.snap.rcp.preferences.layer;
+
+import com.bc.ceres.annotation.STTM;
+import com.bc.ceres.binding.Property;
+import com.bc.ceres.binding.ValidationException;
+import org.esa.snap.core.layer.WorldMapLayerType;
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class WorldMapLayerControllerTest {
+
+    @Test
+    @STTM("SNAP-3762")
+    public void testUpdateProperty_empty() throws ValidationException {
+        final JComboBox<WorldMapLayerType> comboBox = new JComboBox<>();
+        final Property property = mock(Property.class);
+
+        WorldMapLayerController.updateProperty(comboBox, property);
+        verify(property, times(0)).setValue(any());
+    }
+
+    @Test
+    @STTM("SNAP-3762")
+    public void testUpdateProperty() throws ValidationException {
+        final JComboBox<WorldMapLayerType> comboBox = new JComboBox<>();
+        final WorldMapLayerType mapLayerType = mock(WorldMapLayerType.class);
+        when(mapLayerType.getLabel()).thenReturn("woduwolle");
+        comboBox.addItem(mapLayerType);
+
+        final Property property = mock(Property.class);
+
+        WorldMapLayerController.updateProperty(comboBox, property);
+        verify(property, times(1)).setValue("woduwolle");
+    }
+}
