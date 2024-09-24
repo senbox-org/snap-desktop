05ca59dcafe5d6a68c946798b2ce92cb06e277c0
Daniel Knowles <daniel.s.knowles@nasa.gov>
2024-09-24 18:29:29 -0400

    Version Check: updated.  Fixed online help.

--- snap-rcp/src/main/java/org/esa/snap/rcp/util/DesktopVersionCheck.java
+++ snap-rcp/src/main/java/org/esa/snap/rcp/util/DesktopVersionCheck.java
@@ -3,6 +3,8 @@ package org.esa.snap.rcp.util;
 import org.esa.snap.core.util.SystemUtils;
 import org.esa.snap.core.util.VersionChecker;
 import org.esa.snap.rcp.SnapApp;
+import org.esa.snap.rcp.preferences.Preference;
+import org.esa.snap.runtime.EngineConfig;
 import org.openide.awt.CheckForUpdatesProvider;
 import org.openide.util.Lookup;
 import org.openide.windows.OnShowing;
@@ -12,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.Cursor;
+import java.util.prefs.Preferences;
 
 /**
  * @author Marco Peters
@@ -20,10 +23,7 @@ import java.awt.Cursor;
 public class DesktopVersionCheck {
 
     private static final String APPLICATION_HOMEPAGE_URL = SystemUtils.getApplicationHomepageUrl();
-    private static final String MSG_UPDATE_INFO =
-            "<html>A new " + SnapApp.getDefault().getInstanceName() + " version is available for download!<br>" +
-                    "Currently installed " + SnapApp.getDefault().getInstanceName() + " %s, available is " + SnapApp.getDefault().getInstanceName() + " %s.<br>" +
-                    "To download, please visit the " + SnapApp.getDefault().getInstanceName() + " home page at";
+
     private static final VersionChecker VERSION_CHECKER = VersionChecker.getInstance();
 
     private DesktopVersionCheck() {
@@ -47,29 +47,39 @@ public class DesktopVersionCheck {
 
                     String localVersion = String.valueOf(VERSION_CHECKER.getLocalVersion());
                     String remoteVersion = String.valueOf(VERSION_CHECKER.getRemoteVersion());
-                    panel.add(new JLabel(String.format(MSG_UPDATE_INFO + "", localVersion, remoteVersion)));
+
+                    final String message =
+                            "<html><b>" + SnapApp.getDefault().getInstanceName() + " " + remoteVersion + "</b> is now available!<br><br>" +
+                                    "Your current version is: " + SnapApp.getDefault().getInstanceName() + " " + localVersion + "<br><br>" +
+                                    "Version check frequency: " + VERSION_CHECKER.getCheckIntervalString() + "<br>" +
+                                    "- To adjust version check frequency see (Preferences > General > Other)" + "<br><br>" +
+                                    "To download latest SeaDAS, please visit the " + SnapApp.getDefault().getInstanceName() + " home page:" +
+                                    "";
+
+                    panel.add(new JLabel(message));
 
                     final JLabel LinkLabel = new JLabel("<html><a href=\"" + APPLICATION_HOMEPAGE_URL + "\">" + APPLICATION_HOMEPAGE_URL + "</a>");
                     LinkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
                     LinkLabel.addMouseListener(new BrowserUtils.URLClickAdaptor(APPLICATION_HOMEPAGE_URL));
                     panel.add(LinkLabel);
 
-                    JOptionPane.showMessageDialog(null, panel);
+                    JOptionPane.showMessageDialog(null, panel,  "SeaDAS Version Update Notice", JOptionPane.INFORMATION_MESSAGE);
                     return;
+
+                } else {
+//
+//                    String localVersion = String.valueOf(VERSION_CHECKER.getLocalVersion());
+//
+//                    final String message =
+//                            "<html>You are running the latest SeaDAS version: " + localVersion + " <br><br>" +
+//                                    "Version check frequency: " + VERSION_CHECKER.getCheckIntervalString() + "<br>" +
+//                                    "- To adjust version check frequency see (Preferences > General > Other)" + "<br><br>" +
+//                                    "";
+//
+//                    Dialogs.showMessage("Version Check", message, JOptionPane.INFORMATION_MESSAGE, "optional.version.check.onstartup");
+//                    return;
                 }
-            }
-            final String message =
-                    "You are running the latest major version " + VERSION_CHECKER.getLocalVersion() + " of " + SnapApp.getDefault().getInstanceName() +
-                            ".\n" +
-                            "Please check regularly for new plugin updates (Help -> Check for Updates...) \n" +
-                            "to get the best " + SnapApp.getDefault().getInstanceName() + " experience.\n\n" +
-                            "Press 'Yes', if you want to check for plugin updates now.\n\n";
-            Dialogs.Answer decision = Dialogs.requestDecision(SnapApp.getDefault().getInstanceName() + " Update", message, false, "optional.version.check.onstartup");
-            if (Dialogs.Answer.YES.equals(decision)) {
-                final CheckForUpdatesProvider checkForUpdatesProvider = Lookup.getDefault().lookup(CheckForUpdatesProvider.class);
-                checkForUpdatesProvider.openCheckForUpdatesWizard(true);
             }
         }
     }
-
 }
